b51b98ddfecaf8416c5ad3069a05e89ca5a93761
Jefferson Rabb <jeff@atavist.net>
2019-03-31 08:03:06 -0400

    Newspack blocks plugin, initial commit. Built with WPCLI.

---
+++ newspack-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Newspack Blocks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     newspack-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Newspack_Blocks
+ */
+
+// Your code starts here.
