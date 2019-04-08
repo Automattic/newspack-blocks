247543bc5e3824b67ff4ee342488b691e6bb79ef
Jefferson Rabb <jeff@atavist.net>
2019-04-08 18:53:43 -0400

    Remove beta blocks handling.

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,7 @@
 /**
  * External dependencies
  */
-const _ = require( 'lodash' );
 const fs = require( 'fs' );
-const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
 const path = require( 'path' );
 
@@ -23,20 +21,16 @@ const path = require( 'path' );
 const editorSetup = path.join( __dirname, 'src', 'setup', 'editor' );
 const viewSetup = path.join( __dirname, 'src', 'setup', 'view' );
 
-function blockScripts( type, inputDir, presetBlocks ) {
-	return presetBlocks
+function blockScripts( type, inputDir, blocks ) {
+	return blocks
 		.map( block => path.join( inputDir, 'blocks', block, `${ type }.js` ) )
 		.filter( fs.existsSync );
 }
 
-const presetPath = path.join( __dirname, 'src', 'setup', 'blocks.json' );
-const presetIndex = require( presetPath );
-const presetBlocks = _.get( presetIndex, [ 'production' ], [] );
-const presetBetaBlocks = _.get( presetIndex, [ 'beta' ], [] );
-const allPresetBlocks = [ ...presetBlocks, ...presetBetaBlocks ];
+const blocks = require( path.join( __dirname, 'src', 'setup', 'blocks.json' ) );
 
 // Helps split up each block into its own folder view script
-const viewBlocksScripts = allPresetBlocks.reduce( ( viewBlocks, block ) => {
+const viewBlocksScripts = blocks.reduce( ( viewBlocks, block ) => {
 	const viewScriptPath = path.join( __dirname, 'src', 'blocks', block, 'view.js' );
 	if ( fs.existsSync( viewScriptPath ) ) {
 		viewBlocks[ block + '/view' ] = [ viewSetup, ...[ viewScriptPath ] ];
@@ -47,33 +41,15 @@ const viewBlocksScripts = allPresetBlocks.reduce( ( viewBlocks, block ) => {
 // Combines all the different blocks into one editor.js script
 const editorScript = [
 	editorSetup,
-	...blockScripts( 'editor', path.join( __dirname, 'src' ), presetBlocks ),
-];
-
-// Combines all the different blocks into one editor-beta.js script
-const editorBetaScript = [
-	editorSetup,
-	...blockScripts( 'editor', path.join( __dirname, 'src' ), allPresetBlocks ),
+	...blockScripts( 'editor', path.join( __dirname, 'src' ), blocks ),
 ];
 
 const webpackConfig = getBaseWebpackConfig( null, {
 	entry: {
 		editor: editorScript,
-		'editor-beta': editorBetaScript,
 		...viewBlocksScripts,
 	},
 	'output-path': path.join( __dirname, 'dist' ),
 } );
 
-module.exports = {
-	...webpackConfig,
-	plugins: [
-		...webpackConfig.plugins,
-		new CopyWebpackPlugin( [
-			{
-				from: presetPath,
-				to: 'index.json',
-			},
-		] ),
-	],
-};
+module.exports = webpackConfig;
