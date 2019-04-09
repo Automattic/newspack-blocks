210ed4f262b7103e7203af2293a071ea0bcf772b
Jefferson Rabb <j.max.rabb@gmail.com>
2019-04-09 14:37:39 -0400

    Refactor Plugin PHP as a Class (#5)
    * Refactor PHP as a class. Editor files only for now.
    
    * Dependencies from generated file.
    
    * Load view-side render_callback files if present. If style or script is available but render_callback file is not, create generic render_callback to load view-side assets.
    
    * Load view-side dependencies programmatically from generated .deps.json files.
    
    * Remove superfluous comment.
    
    * Add view-side style to Example block.
    
    * Change view-side rendering filename from index.php->view.php.
    
    * Replace "slug" with "type".
    
    * Utility function to create classlist for server-side rendered blocks.
    
    * Example block demonstrating server-side rendering approach.
    
    * Pass in env with WP:true to play nicely with https://github.com/Automattic/wp-calypso/commit/f63ef2d9c13066a0df7b2f6a526dc3486c55290f.

--- newspack-blocks.php
+++ newspack-blocks.php
@@ -15,92 +15,135 @@
 define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', 'dist/' );
 define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEWSPACK_BLOCKS__VERSION', '0.1.0' );
-
 /**
- * Enqueue block scripts and styles for the editor.
+ * Newspack blocks functionality
  */
-function newspack_blocks_enqueue_block_editor_assets() {
-	$editor_script = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.js', __FILE__ );
-	$editor_style  = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.css', __FILE__ );
-	$dependencies  = array(
-		'lodash',
-		'wp-api-fetch',
-		'wp-blob',
-		'wp-blocks',
-		'wp-components',
-		'wp-compose',
-		'wp-data',
-		'wp-date',
-		'wp-edit-post',
-		'wp-editor',
-		'wp-element',
-		'wp-escape-html',
-		'wp-hooks',
-		'wp-i18n',
-		'wp-keycodes',
-		'wp-plugins',
-		'wp-polyfill',
-		'wp-rich-text',
-		'wp-token-list',
-		'wp-url',
-	);
-	wp_enqueue_script(
-		'newspack-blocks-editor',
-		$editor_script,
-		$dependencies,
-		NEWSPACK_BLOCKS__VERSION,
-		true
-	);
-	wp_enqueue_style(
-		'newspack-blocks-editor',
-		$editor_style,
-		array(),
-		NEWSPACK_BLOCKS__VERSION
-	);
-}
-
-/**
- * Enqueue view scripts and styles for a single block.
- *
- * @param string $type The block's slug.
- * @param array  $dependencies An array of script dependencies.
- */
-function newspack_blocks_enqueue_view_assets( $type, $dependencies = array() ) {
-	if ( is_admin() ) {
-		// A block's view assets will not be required in wp-admin.
-		return;
-	}
-	$style_path  = BLOCKS_DIRECTORY . $type . '/view' . ( is_rtl() ? '.rtl' : '' ) . '.css';
-	$script_path = BLOCKS_DIRECTORY . $type . '/view.js';
-
-	if ( file_exists( NEWSPACK__PLUGIN_DIR . $style_path ) ) {
-		wp_enqueue_style(
-			"newspack-block-{$type}",
-			plugins_url( $style_path, __FILE__ ),
-			array(),
-			NEWSPACK_BLOCKS__VERSION
-		);
-	}
-
-	if ( file_exists( NEWSPACK__PLUGIN_DIR . $script_path ) ) {
+class Newspack_Blocks {
+	/**
+	 * Enqueue block scripts and styles for editor.
+	 */
+	public static function enqueue_block_editor_assets() {
+		$editor_script = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.js', __FILE__ );
+		$editor_style  = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.css', __FILE__ );
+		$dependencies  = self::dependencies_from_path( NEWSPACK_BLOCKS__PLUGIN_DIR . 'dist/editor.deps.json' );
 		wp_enqueue_script(
-			"newspack-block-{$type}",
-			plugins_url( $script_path, __FILE__ ),
+			'newspack-blocks-editor',
+			$editor_script,
 			$dependencies,
+			NEWSPACK_BLOCKS__VERSION,
+			true
+		);
+		wp_enqueue_style(
+			'newspack-blocks-editor',
+			$editor_style,
 			array(),
 			NEWSPACK_BLOCKS__VERSION
 		);
 	}
-}
 
-/* An array of slugs for the blocks that require server-side handling (rendering, or asset loading) */
-$newspack_blocks_blocks = array();
+	/**
+	 * Enqueue block scripts and styles for view.
+	 */
+	public static function manage_view_scripts() {
+		if ( is_admin() ) {
+			// In editor environment, do nothing.
+			return;
+		}
+		$src_directory  = NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/blocks/';
+		$dist_directory = NEWSPACK_BLOCKS__PLUGIN_DIR . 'dist/';
+		$iterator       = new DirectoryIterator( $src_directory );
+		foreach ( $iterator as $block_directory ) {
+			if ( ! $block_directory->isDir() || $block_directory->isDot() ) {
+				continue;
+			}
+			$type = $block_directory->getFilename();
 
-foreach ( $newspack_blocks_blocks as $newspack_blocks_block ) {
-	$newspack_blocks_view_path = NEWSPACK__PLUGIN_DIR . 'src/blocks/' . $newspack_blocks_block . '/view.php';
-	if ( file_exists( $newspack_blocks_view_path ) ) {
-		include_once $newspack_blocks_view_path;
+			/* If view.php is found, include it and use for block rendering. */
+			$view_php_path = $src_directory . $type . '/view.php';
+			if ( file_exists( $view_php_path ) ) {
+				include_once $view_php_path;
+				continue;
+			}
+
+			/* If view.php is missing but view Javascript file is found, do generic view asset loading. */
+			$view_js_path = $dist_directory . $type . '/view.js';
+			if ( file_exists( $view_js_path ) ) {
+				register_block_type(
+					"newspack-blocks/{$type}",
+					array(
+						'render_callback' => function( $attributes, $content ) use ( $type ) {
+							Newspack_Blocks::enqueue_view_assets( $type );
+							return $content;
+						},
+					)
+				);
+			}
+		}
+	}
+
+	/**
+	 * Enqueue view scripts and styles for a single block.
+	 *
+	 * @param string $type The block's type.
+	 */
+	public static function enqueue_view_assets( $type ) {
+		$style_path  = NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $type . '/view' . ( is_rtl() ? '.rtl' : '' ) . '.css';
+		$script_path = NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $type . '/view.js';
+		if ( file_exists( NEWSPACK_BLOCKS__PLUGIN_DIR . $style_path ) ) {
+			wp_enqueue_style(
+				"newspack-blocks-{$type}",
+				plugins_url( $style_path, __FILE__ ),
+				array(),
+				NEWSPACK_BLOCKS__VERSION
+			);
+		}
+		if ( file_exists( NEWSPACK_BLOCKS__PLUGIN_DIR . $script_path ) ) {
+			$dependencies = self::dependencies_from_path( NEWSPACK_BLOCKS__PLUGIN_DIR . "dist/{$type}/view.deps.json" );
+			wp_enqueue_script(
+				"newspack-blocks-{$type}",
+				plugins_url( $script_path, __FILE__ ),
+				$dependencies,
+				array(),
+				NEWSPACK_BLOCKS__VERSION
+			);
+		}
+	}
+
+	/**
+	 * Parse generated .deps.json file and return array of dependencies to be enqueued.
+	 *
+	 * @param string $path Path to the generated dependencies file.
+	 *
+	 * @return array Array of dependencides.
+	 */
+	public static function dependencies_from_path( $path ) {
+		$dependencies = file_exists( $path )
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+			? json_decode( file_get_contents( $path ) )
+			: array();
+		$dependencies[] = 'wp-polyfill';
+		return $dependencies;
+	}
+
+	/**
+	 * Utility to assemble the class for a server-side rendered bloc
+	 *
+	 * @param string $type The block type.
+	 * @param array  $attributes Block attributes.
+	 *
+	 * @return string Class list separated by spaces.
+	 */
+	public static function block_classes( $type, $attributes = array() ) {
+		$align   = isset( $attributes['align'] ) ? $attributes['align'] : 'center';
+		$classes = array(
+			"wp-block-newspack-blocks-{$type}",
+			"align{$align}",
+		);
+		if ( isset( $attributes['className'] ) ) {
+			array_push( $classes, $attributes['className'] );
+		}
+		return implode( $classes, ' ' );
 	}
 }
-
-add_action( 'enqueue_block_editor_assets', 'newspack_blocks_enqueue_block_editor_assets' );
+Newspack_Blocks::manage_view_scripts();
+add_action( 'enqueue_block_editor_assets', array( 'Newspack_Blocks', 'enqueue_block_editor_assets' ) );
