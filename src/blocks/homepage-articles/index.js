f0900a4e1d28d16d2ed142e7f5a0ef88181948c2
Laurel Fulford <laurelfulford@gmail.com>
2019-04-17 15:27:40 -0700

    Update block icon to be more representative of the content.

--- src/blocks/homepage-articles/index.js
+++ src/blocks/homepage-articles/index.js
@@ -22,8 +22,8 @@ export const title = __( 'Newspack Homepage Articles' );
 /* From https://material.io/tools/icons */
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-		<Path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
 		<Path d="M0 0h24v24H0z" fill="none" />
+		<Path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" />
 	</SVG>
 );
 
