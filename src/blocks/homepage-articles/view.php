5400761785ab5311b974b6b6f2ef8cc503e0cab3
Laurel Fulford <laurelfulford@gmail.com>
2019-05-12 14:20:43 -0700

    Rework the post output to use WP_Query and remove unneeded  variable; reset post data after loop is completed.

--- src/blocks/homepage-articles/view.php
+++ src/blocks/homepage-articles/view.php
@@ -13,15 +13,15 @@
  * @return string Returns the post content with latest posts added.
  */
 function newspack_blocks_render_block_homepage_articles( $attributes ) {
-	$categories = isset( $attributes['categories'] ) ? $attributes['categories'] : '';
-
-	$args    = array(
+	$categories    = isset( $attributes['categories'] ) ? $attributes['categories'] : '';
+	$args          = array(
 		'posts_per_page'   => $attributes['postsToShow'],
 		'post_status'      => 'publish',
 		'suppress_filters' => false,
 		'cat'              => $categories,
 	);
-	$posts   = get_posts( $args );
+	$article_query = new WP_Query( $args );
+
 	$classes = Newspack_Blocks::block_classes( 'homepage-articles', $attributes );
 
 	if ( isset( $attributes['postLayout'] ) && 'grid' === $attributes['postLayout'] ) {
@@ -39,113 +39,95 @@ function newspack_blocks_render_block_homepage_articles( $attributes ) {
 	if ( $attributes['showImage'] && isset( $attributes['imageScale'] ) ) {
 		$classes .= ' image-scale' . $attributes['imageScale'];
 	}
-
 	if ( isset( $attributes['className'] ) ) {
 		$classes .= ' ' . $attributes['className'];
 	}
 
 	ob_start();
-	?>
-	<div class="<?php echo esc_attr( $classes ); ?>">
-		<?php
-		foreach ( $posts as $post ) :
-			setup_postdata( $post );
-			$featured_image = get_the_post_thumbnail( $post, 'large' );
-			?>
-			<article <?php echo $featured_image ? 'class="article-has-image"' : ''; ?>>
-				<?php
 
-				$image_allowed_tags = array(
-					'img' => array(
-						'alt'    => array(),
-						'class'  => array(),
-						'height' => array(),
-						'src'    => array(),
-						'width'  => array(),
-					),
-				);
-				if ( $featured_image && $attributes['showImage'] ) :
-					?>
-					<div class="article-thumbnail">
-						<?php
-						echo wp_kses( $featured_image, $image_allowed_tags );
-						?>
-					</div><!-- .featured-image -->
-					<?php
-				endif;
+	if ( $article_query->have_posts() ) :
+		?>
+		<div class="<?php echo esc_attr( $classes ); ?>">
+			<?php
+			while ( $article_query->have_posts() ) :
+				$article_query->the_post();
 				?>
-				<div class="article-wrapper">
-					<?php
-					$categories = get_the_category( $post );
-					if ( ! empty( $categories ) && $attributes['showCategory'] ) :
-						?>
-						<div class="cat-links">
-							<a href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ); ?>"><?php echo esc_html( $categories[0]->name ); ?></a>
-						</div>
+				<article <?php echo has_post_thumbnail() ? 'class="article-has-image"' : ''; ?>>
+					<?php if ( has_post_thumbnail() && $attributes['showImage'] ) : ?>
+						<div class="article-thumbnail">
+							<?php the_post_thumbnail( 'large' ); ?>
+						</div><!-- .featured-image -->
+					<?php endif; ?>
+
+					<div class="article-wrapper">
 						<?php
-					endif;
-					?>
-					<h2 class="article-title">
-						<a href="<?php echo esc_url( get_the_permalink( $post ) ); ?>"><?php echo esc_html( get_the_title( $post ) ); ?></a>
-					</h2>
-					<?php
-					if ( $attributes['showExcerpt'] ) :
-						echo '<p>' . esc_html( get_the_excerpt( $post ) ) . '</p>';
-					endif;
-					?>
-
-					<?php if ( $attributes['showAuthor'] || $attributes['showDate'] ) : ?>
-
-						<div class="article-meta">
-
-							<?php if ( $attributes['showAuthor'] ) : ?>
-								<span class="article-byline">
-									<?php
-									if ( $attributes['showAvatar'] ) {
-										echo get_avatar( $post->post_author );
-									}
-									?>
-									<span class="author-name">
-										<?php
-										printf(
-											/* translators: %s: post author. */
-											esc_html_x( 'by %s', 'post author', 'newspack-blocks' ),
-											'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( $post->post_author ) ) . '">' . esc_html( get_the_author_meta( 'display_name', $post->post_author ) ) . '</a></span>'
-										);
-										?>
-									</span><!-- .author-name -->
-								</span><!-- .article-byline -->
-								<?php
-							endif;
-
-							if ( $attributes['showDate'] ) {
-								$time_string = '<time class="article-date published updated" datetime="%1$s">%2$s</time>';
-
-								if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-									$time_string = '<time class="article-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-								}
-
-								$time_string = sprintf(
-									$time_string,
-									esc_attr( get_the_date( DATE_W3C ) ),
-									esc_html( get_the_date() ),
-									esc_attr( get_the_modified_date( DATE_W3C ) ),
-									esc_html( get_the_modified_date() )
-								);
-
-								echo $time_string; // WPCS: XSS OK.
-							}
+						$categories = get_the_category();
+						if ( ! empty( $categories ) && $attributes['showCategory'] ) :
 							?>
-						</div><!-- .article-meta -->
-						<?php
-					endif;
-					?>
-				</div><!-- .article-wrapper -->
-			</article>
+							<div class="cat-links">
+								<a href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ); ?>"><?php echo esc_html( $categories[0]->name ); ?></a>
+							</div><!-- .cat-links -->
+							<?php endif; ?>
 
-		<?php endforeach; ?>
-	</div>
-	<?php
+						<?php the_title( '<h2 class="article-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+
+						<?php if ( $attributes['showExcerpt'] ) : ?>
+							<p><?php the_excerpt(); ?></p>
+						<?php endif; ?>
+
+						<?php if ( $attributes['showAuthor'] || $attributes['showDate'] ) : ?>
+
+							<div class="article-meta">
+
+								<?php if ( $attributes['showAuthor'] ) : ?>
+									<span class="article-byline">
+										<?php
+										if ( $attributes['showAvatar'] ) {
+											echo get_avatar( get_the_author_meta( 'ID' ) );
+										}
+										?>
+										<span class="author-name">
+											<?php
+											printf(
+												/* translators: %s: post author. */
+												esc_html_x( 'by %s', 'post author', 'newspack-blocks' ),
+												'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+											);
+											?>
+										</span><!-- .author-name -->
+									</span><!-- .article-byline -->
+									<?php
+								endif;
+
+								if ( $attributes['showDate'] ) {
+									$time_string = '<time class="article-date published updated" datetime="%1$s">%2$s</time>';
+
+									if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+										$time_string = '<time class="article-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+									}
+
+									$time_string = sprintf(
+										$time_string,
+										esc_attr( get_the_date( DATE_W3C ) ),
+										esc_html( get_the_date() ),
+										esc_attr( get_the_modified_date( DATE_W3C ) ),
+										esc_html( get_the_modified_date() )
+									);
+
+									echo $time_string; // WPCS: XSS OK.
+								}
+								?>
+							</div><!-- .article-meta -->
+						<?php endif; ?>
+					</div><!-- .article-wrapper -->
+				</article>
+				<?php
+			endwhile;
+			wp_reset_postdata();
+			?>
+		</div>
+		<?php
+		endif;
 	$content = ob_get_clean();
 	Newspack_Blocks::enqueue_view_assets( 'homepage-articles' );
 	return $content;
