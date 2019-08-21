31bafdc8a1a07affab6d936ff0bca6d52db624ec
claudiulodro <claudiulodro@gmail.com>
2019-08-21 11:33:38 -0700

    Bump version for release

--- newspack-blocks.php
+++ newspack-blocks.php
@@ -7,14 +7,14 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     newspack-blocks
  * Domain Path:     /languages
- * Version:         1.0.0-alpha.2
+ * Version:         1.0.0-alpha.3
  *
  * @package         Newspack_Blocks
  */
 
 define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', 'dist/' );
 define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.2' );
+define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.3' );
 /**
  * Newspack blocks functionality
  */
