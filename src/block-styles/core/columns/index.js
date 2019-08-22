d565fbc9307ded58fa1934fdc080b9fc707500c6
Laurel Fulford <laurel.fulford@automattic.com>
2019-08-22 13:57:32 -0700

    Add a 'border' style for the columns block.

--- src/block-styles/core/columns/index.js
+++ src/block-styles/core/columns/index.js
@@ -5,11 +5,16 @@ import { registerBlockStyle } from '@wordpress/blocks';
 import './editor.scss';
 
 registerBlockStyle( 'core/columns', {
-  name: 'first-col-to-second',
-  label: 'Move first column to second'
+	name: 'borders',
+	label: 'Borders',
 } );
 
 registerBlockStyle( 'core/columns', {
-  name: 'first-col-to-third',
-  label: 'Move first column to third'
+	name: 'first-col-to-second',
+	label: 'Move first column to second',
+} );
+
+registerBlockStyle( 'core/columns', {
+	name: 'first-col-to-third',
+	label: 'Move first column to third',
 } );
