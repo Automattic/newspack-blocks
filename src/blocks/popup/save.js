32ffb345a3530ae1ebf6b96bcbcf796db0d1cd88
Jefferson Rabb <jeff@atavist.net>
2019-08-27 17:26:41 -0400

    AMP compatible approach to scroll-position popup, using amp-aninmation & amp-position-observer.

--- src/blocks/popup/save.js
+++ src/blocks/popup/save.js
@@ -12,18 +12,7 @@ import { InnerBlocks } from '@wordpress/block-editor';
 class Save extends Component {
 	render() {
 		const { className } = this.props;
-		return (
-			<div className={ className }>
-				<amp-lightbox id="my-lightbox" layout="nodisplay">
-					<div class="lightbox" on="tap:my-lightbox.close" role="button" tabindex="0">
-						<div class="wp-block-newspack-blocks-popup">
-							<InnerBlocks.Content />
-						</div>
-					</div>
-				</amp-lightbox>
-				<button on="tap:my-lightbox">Open lightbox</button>
-			</div>
-		);
+		return <InnerBlocks.Content />;
 	}
 }
 
---
+++ src/blocks/popup/view.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Server-side rendering of the `newspack-blocks/popup` block.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Renders the `newspack-blocks/popup` block on server.
+ *
+ * @param array  $attributes The block attributes.
+ * @param string $content The block content.
+ *
+ * @return string
+ */
+function newspack_blocks_render_block_popup( $attributes, $content ) {
+	ob_start();
+	?>
+	<div class="lightbox" role="button" tabindex="0" id="lightbox1">
+		<div class="wp-block-newspack-blocks-popup">
+			<?php echo wp_kses_post( $content ); ?>
+		</div>
+		<button on="tap:lightbox1.hide" class="lightbox-close">x</button>
+	</div>
+	<div id="marker">
+		<amp-position-observer on="enter:showAnim.start;" once layout="nodisplay" />
+	</div>
+	<amp-animation id="showAnim" layout="nodisplay">
+		<script type="application/json">
+			{
+				"duration": "0",
+				"fill": "both",
+				"iterations": "1",
+				"direction": "alternate",
+				"animations": [{
+					"selector": ".lightbox",
+					"keyframes": [{
+						"transform": "translateX( 0 )",
+						"visibility": "visible"
+					}]
+				}]
+			}
+		</script>
+	</amp-animation>
+	<?php
+	Newspack_Blocks::enqueue_view_assets( 'popup' );
+	return ob_get_clean();
+}
+
+/**
+ * Registers the `newspack-blocks/popup` block on server.
+ */
+function newspack_blocks_register_popup() {
+	register_block_type(
+		'newspack-blocks/popup',
+		array(
+			'attributes'      => array(
+				'className' => array(
+					'type' => 'string',
+				),
+			),
+			'render_callback' => 'newspack_blocks_render_block_popup',
+		)
+	);
+}
+add_action( 'init', 'newspack_blocks_register_popup' );
