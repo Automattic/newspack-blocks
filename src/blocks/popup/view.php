b1c132d17bd4bbc9108b15d622b4f1fd43ea9db8
claudiulodro <claudiulodro@gmail.com>
2019-09-16 17:44:21 -0400

    Record visitor counts for displaying popup

--- src/blocks/popup/view.php
+++ src/blocks/popup/view.php
@@ -14,6 +14,14 @@
  * @return string
  */
 function newspack_blocks_render_block_popup( $attributes, $content ) {
+	if ( ! is_single() ) {
+		return '<!-- Newspack pop-up suppressed -->';
+	}
+
+	if ( newspack_blocks_popup_get_user_visits() > 1000 ) {
+		return '<!-- Newspack pop-up already seen -->';
+	}
+
 	ob_start();
 	?>
 	<div class="lightbox" role="button" tabindex="0" id="lightbox1">
@@ -66,3 +74,25 @@ function newspack_blocks_register_popup() {
 	);
 }
 add_action( 'init', 'newspack_blocks_register_popup' );
+
+/**
+ * Return the number of times the user has visited the site (for determining whether to show pop-ups).
+ *
+ * @return int The number of times.
+ */
+function newspack_blocks_popup_get_user_visits() {
+	return (int) filter_input( INPUT_COOKIE, 'newspack_blocks_popup_user_visits', FILTER_SANITIZE_NUMBER_INT );
+}
+
+/**
+ * Increment the visit count.
+ */
+function newspack_blocks_popup_record_user_visit() {
+	if ( ! is_singular() ) {
+		return;
+	}
+
+	$num_visits = (int) newspack_blocks_popup_get_user_visits();
+	setcookie( 'newspack_blocks_popup_user_visits', 1 + $num_visits, time() + WEEK_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN, is_ssl() );
+}
+add_action( 'wp', 'newspack_blocks_popup_record_user_visit' );
