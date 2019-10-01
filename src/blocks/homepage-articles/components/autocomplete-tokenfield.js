f1378d9f3f3fa743c7dcf191e6035fb1e3705782
claudiulodro <claudiulodro@gmail.com>
2019-10-01 11:48:58 -0700

    Improve suggestion search reliability

--- src/blocks/homepage-articles/components/autocomplete-tokenfield.js
+++ src/blocks/homepage-articles/components/autocomplete-tokenfield.js
@@ -1,9 +1,11 @@
 import { map } from 'lodash';
 
 import { Component } from '@wordpress/element';
-import { FormTokenField } from '@wordpress/components';
+import { FormTokenField, Spinner } from '@wordpress/components';
 import { withState } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
+
+import './autocomplete-tokenfield.scss';
 
 class AutocompleteTokenField extends Component {
 
@@ -13,6 +15,7 @@ class AutocompleteTokenField extends Component {
 			suggestions: [],
 			validValuesByLabel: {},
 			validValuesByValue: {},
+			loading: false,
 		};
 	}
 
@@ -23,17 +26,23 @@ class AutocompleteTokenField extends Component {
 			return;
 		}
 
-		fetchSavedInfo( tokens )
-			.then( results => {
-				const { validValuesByLabel, validValuesByValue }  = this.state;
+		this.setState( { loading: true }, () => {
+			fetchSavedInfo( tokens )
+				.then( results => {
+					const { validValuesByLabel, validValuesByValue }  = this.state;
 
-				results.forEach( suggestion => {
-					validValuesByLabel[ suggestion.label ] = suggestion.value;
-					validValuesByValue[ suggestion.value ] = suggestion.label;
+					results.forEach( suggestion => {
+						validValuesByLabel[ suggestion.label ] = suggestion.value;
+						validValuesByValue[ suggestion.value ] = suggestion.label;
+					} );
+
+					this.setState( { validValuesByLabel, validValuesByValue, loading: false } );
 				} );
+		} );
+	}
 
-				this.setState( { validValuesByLabel, validValuesByValue } );
-			} );
+	componentWillUnmount() {
+		delete this.suggestionsRequest;
 	}
 
 	updateSuggestions( input ) {
@@ -42,8 +51,16 @@ class AutocompleteTokenField extends Component {
 			return;
 		}
 
-		fetchSuggestions( input )
-			.then( suggestions => { 
+		this.setState( { loading: true }, () => {
+			const request = fetchSuggestions( input );
+			request.then( suggestions => { 
+				// A fetch Promise doesn't have an abort option. It's mimicked by
+				// comparing the request reference in on the instance, which is
+				// reset or deleted on subsequent requests or unmounting.
+				if ( this.suggestionsRequest !== request ) {
+					return;
+				}
+
 				const { validValuesByLabel, validValuesByValue }  = this.state;
 				const currentSuggestions = []
 
@@ -53,8 +70,17 @@ class AutocompleteTokenField extends Component {
 					validValuesByValue[ suggestion.value ] = suggestion.label;
 				} );
 
-				this.setState( { suggestions: currentSuggestions, validValuesByLabel, validValuesByValue } );
-			} ); 
+				this.setState( { suggestions: currentSuggestions, validValuesByLabel, validValuesByValue, loading: false } );
+			} ).catch( () => {
+				if ( this.suggestionsRequest === request ) {
+					this.setState( {
+						loading: false,
+					} );
+				}
+			} );
+
+			this.suggestionsRequest = request;
+		} );
 	}
 
 	handleOnChange( tokenStrings ) {
@@ -85,16 +111,19 @@ class AutocompleteTokenField extends Component {
 
 	render() {
 		const { label = '' } = this.props;
-		const { suggestions } = this.state;
+		const { suggestions, loading } = this.state;
 
 		return (
-			<FormTokenField
-				value={ this.getTokens() }
-				suggestions={ suggestions }
-				onChange={ tokens => this.handleOnChange( tokens ) }
-				onInputChange={ input => this.updateSuggestions( input ) }
-				label={ label }
-			/>
+			<div className='autocomplete-tokenfield'>
+				<FormTokenField
+					value={ this.getTokens() }
+					suggestions={ suggestions }
+					onChange={ tokens => this.handleOnChange( tokens ) }
+					onInputChange={ input => this.updateSuggestions( input ) }
+					label={ label }
+				/>
+				{ loading && <Spinner /> }
+			</div>
 		);
 	}
 }
