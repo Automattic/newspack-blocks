41c336541e6033cf1bcc604258d1ac6a0bccda1f
Dave Smith <getdavemail@gmail.com>
2019-11-26 09:37:05 +0000

    Split up render function to allow for alternate render modes.
    It may be that we don’t want to force AMP. Therefore we setup to allow the Articles list to be rendered differently depending on some CONSTANT value.

---
+++ src/blocks/homepage-articles/articles-amp.php
@@ -0,0 +1,30 @@
+<amp-list
+    src="<?php echo esc_url( $articles_rest_url ); ?>"
+    layout="responsive"
+    width="0"
+    height="0"
+    binding="refresh"
+    load-more="manual"
+    load-more-bookmark="next">
+
+    <template type="amp-mustache">
+        {{{html}}}
+    </template>
+    <div fallback>
+        <?php
+        echo newspack_template_inc(__DIR__ . '/articles-loop.php', array(
+            'attributes'    => $attributes,
+            'article_query' => $article_query,
+        ) );
+        ?>
+    </div>
+    <amp-list-load-more load-more-failed>
+        <p><?php esc_html_e('Unable to load articles at this time.');?></p>
+    </amp-list-load-more>
+
+    <?php if ( $attributes['moreButton'] ) : ?>
+    <amp-list-load-more load-more-button class="amp-visible">
+        <button load-more-clickable><?php _e( 'Load more articles' ); ?></button>
+    </amp-list-load-more>
+    <?php endif; ?>
+</amp-list>
