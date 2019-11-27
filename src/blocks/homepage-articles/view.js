ff8d32fb498f80e8a2062d6485f30176002eefd5
Bartek <bartlomiej.kalisz@gmail.com>
2019-11-27 15:37:29 +0100

    Use apiFetch instead of XMLHttpRequest

--- src/blocks/homepage-articles/view.js
+++ src/blocks/homepage-articles/view.js
@@ -4,39 +4,37 @@
  */
 
 /**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+
+/**
  * Style dependencies
  */
 import './view.scss';
 
-
 /**
  * Load More Button Handling
  */
-const loadMoreButton = document.querySelector( '[data-load-more-btn]' );
-const loadMoreButtonURLAttribute = 'data-load-more-url';
+const loadMoreBtn = document.querySelector( '[data-load-more-btn]' );
+const loadMoreBtnURLAttr = 'data-load-more-url';
 const postsContainer = document.querySelector( '[data-posts-container]');
 
-if ( loadMoreButton ) {
-  loadMoreButton.addEventListener( 'click', handleLoadMoreButtonClick );
+if ( loadMoreBtn ) {
+  loadMoreBtn.addEventListener( 'click', handleLoadMoreBtnClick );
 }
 
-function handleLoadMoreButtonClick() {
-  const loadMoreURL = this.getAttribute( loadMoreButtonURLAttribute );
-  const request = new XMLHttpRequest();
+function handleLoadMoreBtnClick() {
+  const loadMoreURL = this.getAttribute( loadMoreBtnURLAttr );
 
-  request.onload = function () {
-    if ( request.status === 200 ) {
-      const payload = JSON.parse( request.responseText );
-
-      renderPosts( payload.items );
-      loadMoreButton.setAttribute( loadMoreButtonURLAttribute, payload.next );
-    } else {
+  apiFetch( { url: loadMoreURL } )
+    .then(( data ) => {
+      renderPosts( data.items );
+      loadMoreBtn.setAttribute( loadMoreBtnURLAttr, data.next );
+    })
+    .catch(( error ) => {
       console.log( 'Something went wrong!' );
-    }
-  };
-
-  request.open( 'GET', loadMoreURL );
-  request.send();
+    });
 };
 
 function renderPosts( items ) {
