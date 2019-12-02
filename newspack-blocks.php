4631e284d1eebe10d5e8fda5d59435fac7993c77
Konstantin Obenland <obenland@gmx.de>
2019-12-02 11:20:00 -0800

    Add REST endpoint from #226

--- newspack-blocks.php
+++ newspack-blocks.php
@@ -19,6 +19,18 @@ define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.17' );
 require_once NEWSPACK_BLOCKS__PLUGIN_DIR . 'class-newspack-blocks.php';
 require_once NEWSPACK_BLOCKS__PLUGIN_DIR . 'class-newspack-blocks-api.php';
 
+// REST Controller for Articles Block.
+require_once NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/blocks/homepage-articles/class-wp-rest-newspack-articles-controller.php';
+
+/**
+ * Registers Articles block routes.
+ */
+function newspack_articles_block_register_rest_routes() {
+	$articles_controller = new WP_REST_Newspack_Articles_Controller();
+	$articles_controller->register_routes();
+}
+add_action( 'rest_api_init', 'newspack_articles_block_register_rest_routes' );
+
 add_action( 'after_setup_theme', array( 'Newspack_Blocks', 'add_image_sizes' ) );
 
 Newspack_Blocks::manage_view_scripts();
