228a61dafe16a86b3c83230c2e3ab215668c9175
Jefferson Rabb <jeff@atavist.net>
2019-12-05 12:49:03 -0500

    Removing NodeList.forEach polyfill. TODO: IE11 compatibility in a future PR.

--- src/blocks/homepage-articles/view.js
+++ src/blocks/homepage-articles/view.js
@@ -13,14 +13,6 @@ import apiFetch from '@wordpress/api-fetch';
  */
 import './view.scss';
 
-/**
- * NodeList.prototype.forEach polyfill for IE.
- * Source: https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
- */
-if (window.NodeList && !NodeList.prototype.forEach) {
-	NodeList.prototype.forEach = Array.prototype.forEach;
-}
-
 const btnURLAttr = 'data-load-more-url';
 const fetchRetryCount = 3;
 
