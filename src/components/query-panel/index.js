7bbe36d039a195dc2edd562ff9ab1e52034bd596
George Hotelling <george.hotelling@automattic.com>
2020-01-30 14:54:34 -0500

    Refactored entity token fields

--- src/components/query-panel/index.js
+++ src/components/query-panel/index.js
@@ -12,114 +12,50 @@ import { withSelect } from '@wordpress/data';
 import { Component, Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-const AuthorField = compose(
-	withState( { authorValues: undefined } ),
-	withSelect( ( select, props ) => {
-		const { onChange, authorValues, setState, value } = props;
-		const allUsers = select( 'core' ).getAuthors();
-
-		const userIdFor = name => allUsers.find( u => u.name === name )?.id;
-		const userNameFor = id => allUsers.find( u => u.id === id )?.name;
-
-		// initialize state from value once select returns
-		if ( ! authorValues && value && allUsers ) {
-			setState( {
-				authorValues: value.map( id => userNameFor( id ) || __( '(no name)', 'newspack-blocks' ) ),
-			} );
-		}
-
-		const updateAuthors = authorValues => {
-			setState( { authorValues } );
-			( onChange || noop )( authorValues.map( userIdFor ) );
-		};
-
-		return {
-			value: authorValues,
-			suggestions: ( allUsers || [] ).map( user => user.name ),
-			onChange: updateAuthors,
-		};
-	} )
-)( props => {
-	return <FormTokenField label={ __( 'Author', 'newspack-blocks' ) } { ...props } />;
-} );
-
-const CategoryField = compose(
+const EntityTokenField = compose(
 	withState( { input: undefined } ),
 	withSelect( ( select, props ) => {
-		const { onChange, setState, value, input } = props;
+		const { entityKind, entityName, onChange, setState, value, input } = props;
 
 		let currentValues = [];
 		if ( value.length ) {
 			currentValues = (
-				select( 'core' ).getEntityRecords( 'taxonomy', 'category', {
+				select( 'core' ).getEntityRecords( entityKind, entityName, {
 					per_page: value.length,
 					include: value.join( ',' ),
 				} ) || []
-			).map( c => ( {
-				value: c.name || __( '(no title)', 'newspack-blocks' ),
-				id: c.id,
+			).map( entity => ( {
+				value: entity.name || '🤷‍♂️',
+				id: entity.id,
 			} ) );
 		}
 
 		const rawSuggestions =
-			select( 'core' ).getEntityRecords( 'taxonomy', 'category', {
+			select( 'core' ).getEntityRecords( entityKind, entityName, {
 				per_page: 100,
 				search: input,
 				exclude: value.join( ',' ),
 			} ) || [];
 
-		const updateCategories = categoryValues => {
+		const updateValue = entityValues => {
 			if ( onChange ) {
-				onChange(
-					categoryValues
-						.map( value =>
-							typeof value === 'object'
-								? value.id
-								: rawSuggestions.find( cat => cat.name == value )?.id
-						)
-						.filter( catId => typeof catId !== 'undefined' )
-				);
+				const entityIds = entityValues
+					.map( value =>
+						typeof value === 'object'
+							? value.id
+							: rawSuggestions.find( cat => cat.name == value )?.id
+					)
+					.filter( catId => typeof catId !== 'undefined' );
+				onChange( entityIds );
 			}
 		};
 
 		return {
-			label: __( 'Category', 'newspack-blocks' ),
+			...props,
 			value: currentValues,
-			suggestions: rawSuggestions.map( c => c.name || __( '(no title)', 'newspack-blocks' ) ),
-			onChange: updateCategories,
+			suggestions: rawSuggestions.map( c => c.name ),
+			onChange: updateValue,
 			onInputChange: input => setState( { input } ),
-		};
-	} )
-)( FormTokenField );
-
-const TagField = compose(
-	withState( { tagValues: undefined } ),
-	withSelect( ( select, props ) => {
-		const { onChange, tagValues, setState, value } = props;
-		const allTags = select( 'core' ).getEntityRecords( 'taxonomy', 'post_tag' );
-
-		const tagIdFor = name => allTags.find( c => c.name === name )?.id;
-		const tagNameFor = id => allTags.find( c => c.id === id )?.name;
-
-		// initialize state from value once select returns
-		if ( ! tagValues && value && allTags?.length > 0 ) {
-			setState( {
-				tagValues: value.map( id => tagNameFor( id ) || __( '(no title)', 'newspack-blocks' ) ),
-			} );
-		}
-
-		const updateTags = tagValues => {
-			setState( { tagValues } );
-			if ( onChange ) {
-				onChange( tagValues.map( tagIdFor ) );
-			}
-		};
-
-		return {
-			label: __( 'Tags', 'newspack-blocks' ),
-			value: tagValues,
-			suggestions: ( allTags || [] ).map( tag => tag.name ),
-			onChange: updateTags,
 		};
 	} )
 )( FormTokenField );
@@ -178,15 +114,27 @@ export default class QueryPanel extends Component {
 				/>
 				{ ! singleMode && (
 					<BaseControl>
-						<AuthorField
+						<EntityTokenField
+							entityKind="root"
+							entityName="user"
+							label={ __( 'Author', 'newspack-blocks' ) }
 							onChange={ author => this.updateCriteria( { author } ) }
 							value={ author }
 						/>
-						<CategoryField
+						<EntityTokenField
+							entityKind="taxonomy"
+							entityName="category"
+							label={ __( 'Category', 'newspack-blocks' ) }
 							onChange={ categories => this.updateCriteria( { categories } ) }
 							value={ categories }
 						/>
-						<TagField onChange={ tags => this.updateCriteria( { tags } ) } value={ tags } />
+						<EntityTokenField
+							entityKind="taxonomy"
+							entityName="post_tag"
+							label={ __( 'tags', 'newspack-blocks' ) }
+							onChange={ tags => this.updateCriteria( { tags } ) }
+							value={ tags }
+						/>
 					</BaseControl>
 				) }
 			</Fragment>
