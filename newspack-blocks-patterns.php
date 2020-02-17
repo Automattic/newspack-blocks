f693647a3cf195475e74b48482bd604363100260
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-02-17 15:55:10 +0000

    Markup: Add 4th pattern to test JSON string escape
    via https://www.freeformatter.com/json-escape.html

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -42,6 +42,22 @@ add_filter(
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
 			$patterns[] = [
+				'category' => __( 'Homepage Articles', 'newspack-blocks' ),
+				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern4.txt'), //phpcs:ignore
+				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
+				'title'    => __( '3 columns', 'newspack-blocks' ),
+			];
+		}
+		return $patterns;
+	},
+	10,
+	2
+);
+add_filter(
+	'newspack_blocks_patterns',
+	function( $patterns, $post_type ) {
+		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
+			$patterns[] = [
 				'category' => __( 'Donations', 'newspack-blocks' ),
 				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern3.txt'), //phpcs:ignore
 				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
