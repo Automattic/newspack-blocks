b266e52a3b4c0b7ff85b5361f37f4d05c9a4b4a3
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-02-19 11:05:27 +0000

    fix: rename homepage articles to homepage posts

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -10,7 +10,7 @@ add_filter(
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
 			$patterns[] = [
-				'category' => __( 'Homepage Articles', 'newspack-blocks' ),
+				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
 				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern1.txt'), //phpcs:ignore
 				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
 				'title'    => __( 'Homepage Layout 1', 'newspack-blocks' ),
@@ -26,7 +26,7 @@ add_filter(
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
 			$patterns[] = [
-				'category' => __( 'Homepage Articles', 'newspack-blocks' ),
+				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
 				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern2.txt'), //phpcs:ignore
 				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
 				'title'    => __( 'Homepage Layout 2', 'newspack-blocks' ),
@@ -41,11 +41,11 @@ add_filter(
 	'newspack_blocks_patterns',
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
-			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern4.json'), true ); //phpcs:ignore
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/homepage-posts/3-columns.json'), true ); //phpcs:ignore
 			$patterns[] = [
-				'category' => __( 'Homepage Articles', 'newspack-blocks' ),
+				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
 				'content'  => $from_json['content'],
-				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
+				'icon'     => plugins_url( 'src/patterns/icons/homepage-posts/3-columns.png', __FILE__ ),
 				'title'    => __( '3 columns', 'newspack-blocks' ),
 			];
 		}
