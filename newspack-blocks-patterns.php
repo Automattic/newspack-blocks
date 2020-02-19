0c4d458aeeaa88b2f4a91333a43c266c3406b0c1
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-02-19 11:45:04 +0000

    feat: add new 2 columns block pattern, reorganise donations patterns

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -5,31 +5,19 @@
  * @package Newspack_Blocks
  */
 
+/**
+ * Homepage Posts.
+ */
 add_filter(
 	'newspack_blocks_patterns',
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/homepage-posts/2-columns.json'), true ); //phpcs:ignore
 			$patterns[] = [
 				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
-				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern1.txt'), //phpcs:ignore
-				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
-				'title'    => __( 'Homepage Layout 1', 'newspack-blocks' ),
-			];
-		}
-		return $patterns;
-	},
-	10,
-	2
-);
-add_filter(
-	'newspack_blocks_patterns',
-	function( $patterns, $post_type ) {
-		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
-			$patterns[] = [
-				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
-				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern2.txt'), //phpcs:ignore
-				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
-				'title'    => __( 'Homepage Layout 2', 'newspack-blocks' ),
+				'content'  => $from_json['content'],
+				'icon'     => plugins_url( 'src/patterns/icons/homepage-posts/2-columns.png', __FILE__ ),
+				'title'    => __( 'Two columns; two-thirds, one-third split', 'newspack-blocks' ),
 			];
 		}
 		return $patterns;
@@ -46,7 +34,7 @@ add_filter(
 				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
 				'content'  => $from_json['content'],
 				'icon'     => plugins_url( 'src/patterns/icons/homepage-posts/3-columns.png', __FILE__ ),
-				'title'    => __( '3 columns', 'newspack-blocks' ),
+				'title'    => __( 'Three columns; equal split', 'newspack-blocks' ),
 			];
 		}
 		return $patterns;
@@ -54,13 +42,17 @@ add_filter(
 	10,
 	2
 );
+
+/**
+ * Donations.
+ */
 add_filter(
 	'newspack_blocks_patterns',
 	function( $patterns, $post_type ) {
 		if ( in_array( $post_type, [ 'post', 'page' ], true ) ) {
 			$patterns[] = [
 				'category' => __( 'Donations', 'newspack-blocks' ),
-				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/pattern3.txt'), //phpcs:ignore
+				'content'  => file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/layout-1.txt'), //phpcs:ignore
 				'icon'     => plugins_url( 'src/patterns/icons/pattern-icon.png', __FILE__ ),
 				'title'    => __( 'Donate Layout 1', 'newspack-blocks' ),
 			];
