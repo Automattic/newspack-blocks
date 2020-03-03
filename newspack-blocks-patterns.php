1407f16d898e36976881d179c25c91ba51babb39
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-03-03 18:13:09 +0000

    feat: add donation patterns

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -190,3 +190,60 @@ add_filter(
 	10,
 	2
 );
+
+/**
+ * Donation.
+ */
+add_filter(
+	'newspack_blocks_patterns',
+	function( $patterns, $post_type ) {
+		if ( in_array( $post_type, [ 'page', 'post', 'newspack_popups_cpt' ], true ) ) {
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/donation/style-1.json'), true ); //phpcs:ignore
+			$patterns[] = [
+				'category' => __( 'Donation', 'newspack-blocks' ),
+				'content'  => $from_json['content'],
+				'image'    => plugins_url( 'src/patterns/images/donation/style-1.png', __FILE__ ),
+				'title'    => __( 'Style 1', 'newspack-blocks' ),
+			];
+		}
+		return $patterns;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_blocks_patterns',
+	function( $patterns, $post_type ) {
+		if ( in_array( $post_type, [ 'page', 'post', 'newspack_popups_cpt' ], true ) ) {
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/donation/style-2.json'), true ); //phpcs:ignore
+			$patterns[] = [
+				'category' => __( 'Donation', 'newspack-blocks' ),
+				'content'  => $from_json['content'],
+				'image'    => plugins_url( 'src/patterns/images/donation/style-2.png', __FILE__ ),
+				'title'    => __( 'Style 2', 'newspack-blocks' ),
+			];
+		}
+		return $patterns;
+	},
+	10,
+	2
+);
+
+add_filter(
+	'newspack_blocks_patterns',
+	function( $patterns, $post_type ) {
+		if ( in_array( $post_type, [ 'page', 'post', 'newspack_popups_cpt' ], true ) ) {
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/donation/style-3.json'), true ); //phpcs:ignore
+			$patterns[] = [
+				'category' => __( 'Donation', 'newspack-blocks' ),
+				'content'  => $from_json['content'],
+				'image'    => plugins_url( 'src/patterns/images/donation/style-3.png', __FILE__ ),
+				'title'    => __( 'Style 3', 'newspack-blocks' ),
+			];
+		}
+		return $patterns;
+	},
+	10,
+	2
+);
