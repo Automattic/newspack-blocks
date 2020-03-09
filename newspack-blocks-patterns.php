cda65328adc70bee88adcec320d51574710e9864
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-03-09 13:25:49 +0000

    feat: homepage posts block pattern add new 4 columns option

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -242,6 +242,24 @@ add_filter(
 	2
 );
 
+add_filter(
+	'newspack_blocks_patterns',
+	function( $patterns, $post_type ) {
+		if ( in_array( $post_type, [ 'page' ], true ) ) {
+			$from_json = json_decode( file_get_contents( NEWSPACK_BLOCKS__PLUGIN_DIR . 'src/patterns/markup/homepage-posts/style-14.json'), true ); //phpcs:ignore
+			$patterns[] = [
+				'category' => __( 'Homepage Posts', 'newspack-blocks' ),
+				'content'  => $from_json['content'],
+				'image'    => plugins_url( 'src/patterns/images/homepage-posts/style-14.png', __FILE__ ),
+				'title'    => __( 'Style 14', 'newspack-blocks' ),
+			];
+		}
+		return $patterns;
+	},
+	10,
+	2
+);
+
 /**
  * Subscribe.
  */
