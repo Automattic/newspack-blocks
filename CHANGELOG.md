53b3bffd5b2d604576855ddff96783794b33a815
matticbot <semantic-release-bot@martynus.net>
2020-04-29 19:43:45 +0000

    chore(release): 1.3.1 [skip ci]
    ## [1.3.1](https://github.com/Automattic/newspack-blocks/compare/v1.3.0...v1.3.1) (2020-04-29)
    
    ### Bug Fixes
    
    * add isset to avoid warnings ([8c791da](https://github.com/Automattic/newspack-blocks/commit/8c791da4a50a5f979a13feee9c7de43a92bf7fc0))

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## [1.3.1](https://github.com/Automattic/newspack-blocks/compare/v1.3.0...v1.3.1) (2020-04-29)
+
+
+### Bug Fixes
+
+* add isset to avoid warnings ([8c791da](https://github.com/Automattic/newspack-blocks/commit/8c791da4a50a5f979a13feee9c7de43a92bf7fc0))
+
 # [1.3.0](https://github.com/Automattic/newspack-blocks/compare/v1.2.1...v1.3.0) (2020-04-21)
 
 
--- newspack-blocks.php
+++ newspack-blocks.php
@@ -7,14 +7,14 @@
  * Author URI:      https://newspack.blog/
  * Text Domain:     newspack-blocks
  * Domain Path:     /languages
- * Version:         1.3.0
+ * Version:         1.3.1
  *
  * @package         Newspack_Blocks
  */
 
 define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', 'dist/' );
 define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'NEWSPACK_BLOCKS__VERSION', '1.3.0' );
+define( 'NEWSPACK_BLOCKS__VERSION', '1.3.1' );
 
 require_once NEWSPACK_BLOCKS__PLUGIN_DIR . 'class-newspack-blocks.php';
 require_once NEWSPACK_BLOCKS__PLUGIN_DIR . 'class-newspack-blocks-api.php';
