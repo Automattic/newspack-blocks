9d130d4db3c007f29dd26059b9e99c93e8722720
Joseph Scott <joseph@josephscott.org>
2020-05-26 15:34:22 -0600

    Update src/blocks/carousel/view.php
    Co-authored-by: Jefferson Rabb <j.max.rabb@gmail.com>

--- src/blocks/carousel/view.php
+++ src/blocks/carousel/view.php
@@ -15,7 +15,7 @@
 function newspack_blocks_render_block_carousel( $attributes ) {
 	static $newspack_blocks_carousel_id = 0;
 
-	// This will let the FSE plugin know we need CSS/JS now
+	// This will let the FSE plugin know we need CSS/JS now.
 	do_action( 'newspack_blocks_render_post_carousel' );
 
 	$newspack_blocks_carousel_id++;
