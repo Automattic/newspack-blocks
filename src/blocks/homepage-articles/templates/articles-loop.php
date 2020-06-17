f25a503f5fd692ae92c7a30bc3b35684b62b4d4b
Jefferson Rabb <jeff@atavist.net>
2020-06-17 18:28:28 -0400

    refactor: remove experimental flag for frontend exclusion queries

--- src/blocks/homepage-articles/templates/articles-loop.php
+++ src/blocks/homepage-articles/templates/articles-loop.php
@@ -23,9 +23,6 @@ call_user_func(
 		$post_counter = 0;
 		while ( $article_query->have_posts() ) {
 			$article_query->the_post();
-			if ( ! Newspack_Blocks::is_experimental_mode() && ! $attributes['specificMode'] && ( isset( $newspack_blocks_post_id[ get_the_ID() ] ) || $post_counter >= $attributes['postsToShow'] ) ) {
-				continue;
-			}
 			$newspack_blocks_post_id[ get_the_ID() ] = true;
 			$post_counter++;
 			echo Newspack_Blocks::template_inc( __DIR__ . '/article.php', array( 'attributes' => $attributes ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
