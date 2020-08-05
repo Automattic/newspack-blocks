ada07ad10eabd984be750c2f370beaf94ecc9134
Laurel Fulford <laurel.fulford@automattic.com>
2020-08-05 16:41:54 -0700

    feat: make sure date is still displayed

--- src/blocks/homepage-articles/templates/article.php
+++ src/blocks/homepage-articles/templates/article.php
@@ -157,27 +157,27 @@ call_user_func(
 							</span><!-- .author-name -->
 							<?php
 						endif;
-						if ( $attributes['showDate'] ) :
-							$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-							if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) :
-								$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-							endif;
-							printf(
-								wp_kses(
-									$time_string,
-									array(
-										'time' => array(
-											'class'    => true,
-											'datetime' => true,
-										),
-									)
-								),
-								esc_attr( get_the_date( DATE_W3C ) ),
-								esc_html( get_the_date() ),
-								esc_attr( get_the_modified_date( DATE_W3C ) ),
-								esc_html( get_the_modified_date() )
-							);
+					endif;
+					if ( $attributes['showDate'] ) :
+						$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+						if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) :
+							$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 						endif;
+						printf(
+							wp_kses(
+								$time_string,
+								array(
+									'time' => array(
+										'class'    => true,
+										'datetime' => true,
+									),
+								)
+							),
+							esc_attr( get_the_date( DATE_W3C ) ),
+							esc_html( get_the_date() ),
+							esc_attr( get_the_modified_date( DATE_W3C ) ),
+							esc_html( get_the_modified_date() )
+						);
 					endif;
 					?>
 				</div><!-- .entry-meta -->
