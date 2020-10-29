1a5550d5e42c1773c3bd5e12014e5f1c2908bbfc
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-10-29 16:25:08 +0000

    feat: update blocks icons style and colour
    Using Material's outlined instead of filled and add Newspack Blue colour to it.

--- src/blocks/homepage-articles/index.js
+++ src/blocks/homepage-articles/index.js
@@ -32,7 +32,10 @@ export const title = __( 'Homepage Posts', 'newspack-blocks' );
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 		<Path d="M0 0h24v24H0z" fill="none" />
-		<Path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zM10 9h8v2h-8zm0 3h4v2h-4zm0-6h8v2h-8z" />
+		<Path
+			d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zM10 9h8v2h-8zm0 3h4v2h-4zm0-6h8v2h-8z"
+			fill="#36f"
+		/>
 	</SVG>
 );
 
