f4d68cb74210e28cec17deec1d16b35ede68e901
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-10-30 08:09:52 +0000

    feat: update blocks icons style and colour (#615)
    Using Material's outlined instead of filled and add Newspack Blue colour to it.

--- src/blocks/video-playlist/index.js
+++ src/blocks/video-playlist/index.js
@@ -20,8 +20,11 @@ export const title = __( 'YouTube Video Playlist', 'newspack-blocks' );
 /* From https://material.io/tools/icons */
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M20 8H4V6h16v2zm-2-6H6v2h12V2zm4 10v8c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-8c0-1.1.9-2 2-2h16c1.1 0 2 .9 2 2zm-6 4l-6-3.27v6.53L16 16z" />
 		<Path d="M0 0h24v24H0z" fill="none" />
+		<Path
+			d="M20 8H4V6h16v2zm-2-6H6v2h12V2zm4 10v8c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-8c0-1.1.9-2 2-2h16c1.1 0 2 .9 2 2zm-6 4l-6-3.27v6.53L16 16z"
+			fill="#36f"
+		/>
 	</SVG>
 );
 
