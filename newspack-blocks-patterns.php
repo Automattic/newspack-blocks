fe462eed5fb0301a60a703f408754e6a88191816
Jefferson Rabb <jeff@atavist.net>
2020-12-07 22:58:00 -0500

    chore: various phpcs fixes

--- newspack-blocks-patterns.php
+++ newspack-blocks-patterns.php
@@ -1,21 +1,26 @@
 <?php
 /**
+ * Newspack block patterns
+ *
+ * @package Newspack_Blocks
+ */
+
+/**
  * Registers block patterns and categories.
  *
  * @package Newspack_Blocks
  */
-function newspack_register_block_patterns_and_categories() {
-
+function newspack_blocks_register_block_patterns_and_categories() {
 	/*
- 	 * Register block patterns.
- 	 */
+	* Register block patterns.
+	*/
 	if ( function_exists( 'register_block_pattern' ) ) {
 		/*
 		 * Register block patterns for particular post types.
 		 * We need to get the post type using the post ID from $_REQUEST
 		 * since the global $post is not available inside the admin_init hook.
 		 */
-		$post_id   = isset( $_REQUEST[ 'post' ] ) ? sanitize_text_field( $_REQUEST[ 'post' ] ) : null;
+		$post_id   = isset( $_REQUEST['post'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['post'] ) ) : null; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		$post_type = ! empty( $post_id ) ? get_post_type( $post_id ) : null;
 
 		/*
@@ -25,7 +30,7 @@ function newspack_register_block_patterns_and_categories() {
 		$donations_configured = false;
 		if ( class_exists( 'Newspack\Donations' ) ) {
 			$settings = Newspack\Donations::get_donation_settings();
-			if ( ! is_wp_error( $settings ) && $settings[ 'created' ] ) {
+			if ( ! is_wp_error( $settings ) && $settings['created'] ) {
 				$donations_configured = true;
 			}
 		}
@@ -41,17 +46,17 @@ function newspack_register_block_patterns_and_categories() {
 
 		$block_patterns = array();
 
-		// Donations
+		// Donations.
 		if ( in_array( $post_type, [ 'page', 'post', 'newspack_popups_cpt' ], true ) && $donations_configured ) {
 			array_push(
 				$block_patterns,
 				'donations-1',
 				'donations-2',
-				'donations-3',
+				'donations-3'
 			);
 		}
 
-		// Homepage Posts
+		// Homepage Posts.
 		if ( in_array( $post_type, [ 'page' ], true ) ) {
 			array_push(
 				$block_patterns,
@@ -74,18 +79,18 @@ function newspack_register_block_patterns_and_categories() {
 				'homepage-posts-17',
 				'homepage-posts-18',
 				'homepage-posts-19',
-				'homepage-posts-20',
+				'homepage-posts-20'
 			);
 		}
 
-		// Subscribe
+		// Subscribe.
 		if ( in_array( $post_type, [ 'page', 'post', 'newspack_popups_cpt' ], true ) && $mailchimp_available ) {
 			array_push(
 				$block_patterns,
 				'subscribe-1',
 				'subscribe-2',
 				'subscribe-3',
-				'subscribe-4',
+				'subscribe-4'
 			);
 		}
 
@@ -98,8 +103,8 @@ function newspack_register_block_patterns_and_categories() {
 	}
 
 	/*
- 	 * Register block pattern categories.
- 	 */
+	* Register block pattern categories.
+	*/
 	if ( function_exists( 'register_block_pattern_category' ) ) {
 		register_block_pattern_category(
 			'newspack-donations',
@@ -114,6 +119,5 @@ function newspack_register_block_patterns_and_categories() {
 			array( 'label' => __( 'Newspack Subscribe', 'newspack-blocks' ) )
 		);
 	}
-
 }
-add_action( 'admin_init', 'newspack_register_block_patterns_and_categories', 10 );
+add_action( 'admin_init', 'newspack_blocks_register_block_patterns_and_categories', 10 );
