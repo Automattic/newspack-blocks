Automattic/newspack-blocks

commit 76f90217abaed19a060f2ea0cc7823bc8654d72f

refactor(iframe): clean edit from unnecessary states

src/blocks/iframe/edit.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { Fragment, useState, useEffect } from '@wordpress/element';
+import { Fragment, useState } from '@wordpress/element';
 import { InspectorControls, BlockControls } from '@wordpress/block-editor';
 import {
 	PanelBody,
@@ -34,12 +34,9 @@ const iconPreview = (
 const IframeEdit = ( { attributes, setAttributes } ) => {
 	const label = __( 'Iframe', 'block title' );
 	const { src, archiveFolder, isFullScreen, height, width } = attributes;
-	const [ formSrc, setFormSrc ] = useState( src );
 	const [ showPreview, setShowPreview ] = useState( true );
 	const [ isUploadingArchive, setIsUploadingArchive ] = useState();
-	const [ archiveFile, setArchiveFile ] = useState();
 	const [ error, setError ] = useState();
-	const [ removeCurrentArchiveFolder, setRemoveCurrentArchiveFolder ] = useState();
 
 	const sizeUnits = [
 		{ value: 'px', label: 'px' },
@@ -54,17 +51,11 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 
 		// remove current archive folder if exists.
 		if ( archiveFolder ) {
-			setRemoveCurrentArchiveFolder( true );
+			deleteIframeArchive();
 		}
 	};
 
-	useEffect( () => {
-		if ( archiveFile ) {
-			uploadIframeArchive();
-		}
-	}, [ archiveFile ] );
-
-	const uploadIframeArchive = async () => {
+	const uploadIframeArchive = async archiveFile => {
 		setError( null );
 		setIsUploadingArchive( true );
 
@@ -80,7 +71,6 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 			} );
 
 			setAttributes( { src: iframeArchiveSrc, archiveFolder: iframeArchiveFolder } );
-			setFormSrc( iframeArchiveSrc );
 			setShowPreview( true );
 		} catch ( e ) {
 			setError(
@@ -106,7 +96,6 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 			} );
 
 			setAttributes( { src: iframeArchiveSrc, archiveFolder: iframeArchiveFolder } );
-			setFormSrc( iframeArchiveSrc );
 			setShowPreview( true );
 		} catch ( e ) {
 			setError(
@@ -121,22 +110,16 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 		setIsUploadingArchive( false );
 	};
 
-	useEffect( () => {
+	const deleteIframeArchive = () => {
 		// Do not remove archive if it's used (if a user clicks on embed after uploading the archive.)
-		if ( removeCurrentArchiveFolder && src && archiveFolder && ! src.includes( archiveFolder ) ) {
-			const formData = new FormData();
-			formData.append( 'archive_folder', archiveFolder );
-
+		if ( src && archiveFolder && ! src.includes( archiveFolder ) ) {
 			apiFetch( {
 				path: '/newspack-blocks/v1/newspack-blocks-remove-iframe-archive',
-				method: 'POST',
-				body: formData,
-			} );
-
-			setAttributes( { archiveFolder: null } );
-			setRemoveCurrentArchiveFolder( false );
+				method: 'DELETE',
+				body: JSON.stringify( { archive_folder: archiveFolder } ),
+			} ); //.then( () => setAttributes( { archiveFolder: null } ) );
 		}
-	}, [ src, archiveFolder ] );
+	};
 
 	const iframeControls = [
 		{
@@ -177,13 +160,13 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 				<IframePlaceholder
 					icon={ iframeIcon }
 					label={ label }
-					src={ formSrc }
-					setFormSrc={ setFormSrc }
+					src={ src }
+					setSrc={ _src => setAttributes( { src: _src } ) }
 					onSelectURL={ embedURL }
 					onSelectMedia={ setIframeArchiveFromMedia }
 					isUploadingArchive={ isUploadingArchive }
 					archiveFolder={ archiveFolder }
-					setArchiveFile={ setArchiveFile }
+					uploadIframeArchive={ uploadIframeArchive }
 					error={ error }
 				/>
 			) }

src/blocks/iframe/iframe-placeholder.js
@@ -37,19 +37,19 @@ const IframePlaceholder = ( {
 	icon,
 	label,
 	src,
-	setFormSrc,
+	setSrc,
 	onSelectURL,
 	onSelectMedia,
 	isUploadingArchive,
 	archiveFolder,
-	setArchiveFile,
+	uploadIframeArchive,
 	error,
 } ) => {
 	const [ isURLInputVisible, setIsURLInputVisible ] = useState( false );
 
 	// URL text field
 	const onChangeSrc = event => {
-		setFormSrc( event.target.value );
+		setSrc( event.target.value );
 	};
 
 	const openURLInput = () => {
@@ -75,7 +75,7 @@ const IframePlaceholder = ( {
 	const onUpload = event => {
 		const files = event.target.files;
 		if ( 0 < files.length ) {
-			setArchiveFile( files.item( 0 ) );
+			uploadIframeArchive( files.item( 0 ) );
 		}
 	};
 

commit c2638d3624ee29e5c2c62c55da11a38ac3bb79da

fix(iframe): fix typing Iframe URL issue
A bug was introduced in 76f9021 where when we type the first letter in the URL field it's submitted,
moved the field state to the right place.

src/blocks/iframe/edit.js
@@ -161,7 +161,6 @@ const IframeEdit = ( { attributes, setAttributes } ) => {
 					icon={ iframeIcon }
 					label={ label }
 					src={ src }
-					setSrc={ _src => setAttributes( { src: _src } ) }
 					onSelectURL={ embedURL }
 					onSelectMedia={ setIframeArchiveFromMedia }
 					isUploadingArchive={ isUploadingArchive }

src/blocks/iframe/iframe-placeholder.js
@@ -37,7 +37,6 @@ const IframePlaceholder = ( {
 	icon,
 	label,
 	src,
-	setSrc,
 	onSelectURL,
 	onSelectMedia,
 	isUploadingArchive,
@@ -46,10 +45,11 @@ const IframePlaceholder = ( {
 	error,
 } ) => {
 	const [ isURLInputVisible, setIsURLInputVisible ] = useState( false );
+	const [ urlFieldValue, setUrlFieldValue ] = useState( src );
 
 	// URL text field
-	const onChangeSrc = event => {
-		setSrc( event.target.value );
+	const onChangeUrlField = event => {
+		setUrlFieldValue( event.target.value );
 	};
 
 	const openURLInput = () => {
@@ -61,8 +61,8 @@ const IframePlaceholder = ( {
 
 	const onSubmitSrc = event => {
 		event.preventDefault();
-		if ( src && onSelectURL ) {
-			onSelectURL( src );
+		if ( urlFieldValue && onSelectURL ) {
+			onSelectURL( urlFieldValue );
 			closeURLInput();
 		}
 	};
@@ -111,8 +111,8 @@ const IframePlaceholder = ( {
 					</Button>
 					{ isURLInputVisible && (
 						<InsertFromURLPopover
-							src={ src }
-							onChange={ onChangeSrc }
+							src={ urlFieldValue }
+							onChange={ onChangeUrlField }
 							onSubmit={ onSubmitSrc }
 							onClose={ closeURLInput }
 						/>