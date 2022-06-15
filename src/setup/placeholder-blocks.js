5e4dfe2c05e0189de5eec9dce4049aa6271c27a0
Thomas Guillot <thomasguillot@users.noreply.github.com>
2022-06-15 11:19:27 +0100

    feat: update placeholder style

--- src/setup/placeholder-blocks.js
+++ src/setup/placeholder-blocks.js
@@ -3,18 +3,21 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { getBlockType, registerBlockType } from '@wordpress/blocks';
-import { ButtonGroup, Button } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { withDispatch } from '@wordpress/data';
+import { Icon, pullquote } from '@wordpress/icons';
 
 const placeholderBlocks = {
 	'newspack-ads/ad-unit': {
-		title: __( 'Newspack Ad Unit', 'newspack-blocks' ),
+		title: __( 'Ad Unit', 'newspack-blocks' ),
+		description: __( 'Render an ad unit from your inventory.', 'newspack-blocks' ),
+		icon: pullquote,
 		message: __( 'Place ad units inside your page by installing Newspack Ads.', 'newspack-blocks' ),
 		url: 'https://github.com/Automattic/newspack-ads',
 	},
 };
 
-function registerPlaceholderBlock( blockName, { title, message, url } ) {
+function registerPlaceholderBlock( blockName, { title, description, icon, message, url } ) {
 	if ( getBlockType( blockName ) ) {
 		return;
 	}
@@ -22,29 +25,37 @@ function registerPlaceholderBlock( blockName, { title, message, url } ) {
 	const edit = ( { clientId, removeBlocks } ) => {
 		return (
 			<div className="newspack-blocks-placeholder-block wp-block">
-				<h4>
-					{ sprintf(
-						// translators: %s is the block name.
-						__( 'The "%s" block is currently not available.', 'newspack-blocks' ),
-						title
-					) }
-				</h4>
+				<div className="newspack-blocks-placeholder-block__label">
+					{ icon && <Icon icon={ icon } /> }
+					{ title }
+				</div>
+				<p>
+					<strong>
+						{ sprintf(
+							// translators: %s is the block name.
+							__( 'The "%s" block is currently not available.', 'newspack-blocks' ),
+							title
+						) }
+					</strong>
+				</p>
 				{ message && <p>{ message }</p> }
-				<ButtonGroup>
-					<Button onClick={ () => removeBlocks( clientId ) }>
-						{ __( 'Remove block', 'newspack-blocks' ) }
-					</Button>
+				<div className="newspack-blocks-placeholder-block__buttons">
 					{ url && (
-						<Button isPrimary target="_blank" rel="external" href={ url }>
-							{ __( 'Visit the plugin page', 'newspack-blocks' ) }
+						<Button variant="primary" target="_blank" rel="external" href={ url }>
+							{ __( 'Visit Plugin Page', 'newspack-blocks' ) }
 						</Button>
 					) }
-				</ButtonGroup>
+					<Button variant="secondary" isDestructive onClick={ () => removeBlocks( clientId ) }>
+						{ __( 'Remove Block', 'newspack-blocks' ) }
+					</Button>
+				</div>
 			</div>
 		);
 	};
 	registerBlockType( blockName, {
 		title,
+		description,
+		icon,
 		edit: withDispatch( dispatch => ( {
 			removeBlocks: dispatch( 'core/block-editor' ).removeBlocks,
 		} ) )( edit ),
