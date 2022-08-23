a2a4fffa95a9608fc502a410b8c2086d949fe052
Derrick Koo <dkoo@users.noreply.github.com>
2022-08-23 15:13:49 -0600

    refactor: handle reCAPTCHA settings and methods move to RAS (#1244)
    * feat: handle reCAPTCHA settings and methods move to RAS
    
    * chore: remove unneeded global type definition
    
    * fix: text domain missing hyphen
    
    * fix: check that Reader_Activation class exists before using its methods
    
    * refactor: handle move of reCAPTCHA to own class
    
    * refactor: rename recaptcha constants
    
    * refactor: esc_html instead of wp_strip_all_tags

--- src/blocks/donate/class-wp-rest-newspack-donate-controller.php
+++ src/blocks/donate/class-wp-rest-newspack-donate-controller.php
@@ -31,14 +31,14 @@ class WP_REST_Newspack_Donate_Controller extends WP_REST_Controller {
 	 * @access public
 	 */
 	public function register_routes() {
-		self::$current_user_id = get_current_user_id();
+		self::$current_user_id = \get_current_user_id();
 
-		register_rest_route(
+		\register_rest_route(
 			$this->namespace,
 			'/' . $this->rest_base,
 			[
 				[
-					'methods'             => WP_REST_Server::EDITABLE,
+					'methods'             => \WP_REST_Server::EDITABLE,
 					'callback'            => [ $this, 'api_process_donation' ],
 					'args'                => [
 						'captchaToken'      => [
@@ -99,67 +99,20 @@ class WP_REST_Newspack_Donate_Controller extends WP_REST_Controller {
 	 * @return WP_REST_Response
 	 */
 	public function api_process_donation( $request ) {
-		// If reCaptcha is available, verify the user action.
-		$use_captcha = \Newspack\Stripe_Connection::can_use_captcha();
-		if ( $use_captcha ) {
-			$captcha_token = $request->get_param( 'captchaToken' );
-			if ( ! $captcha_token ) {
-				return rest_ensure_response(
+		if ( method_exists( '\Newspack\Recaptcha', 'can_use_captcha' ) && \Newspack\Recaptcha::can_use_captcha() ) {
+			$captcha_token  = $request->get_param( 'captchaToken' );
+			$captcha_result = \Newspack\Recaptcha::verify_captcha( $captcha_token );
+			if ( \is_wp_error( $captcha_result ) ) {
+				return \rest_ensure_response(
 					[
-						'error' => __( 'Missing or invalid captcha token.', 'newspack-blocks' ),
-					]
-				);
-			}
-
-			$stripe_settings = \Newspack\Stripe_Connection::get_stripe_data();
-			$captcha_secret  = $stripe_settings['captchaSiteSecret'];
-			$captcha_verify  = wp_safe_remote_post(
-				add_query_arg(
-					[
-						'secret'   => $captcha_secret,
-						'response' => $captcha_token,
-					],
-					'https://www.google.com/recaptcha/api/siteverify'
-				)
-			);
-
-			// If the reCaptcha verification request fails.
-			if ( is_wp_error( $captcha_verify ) ) {
-				return rest_ensure_response(
-					[
-						'error' => wp_strip_all_tags( $captcha_verify->get_error_message() ),
-					]
-				);
-			}
-
-			$captcha_verify = json_decode( $captcha_verify['body'], true );
-
-			// If the reCaptcha verification request succeeds, but with error.
-			if ( ! boolval( $captcha_verify['success'] ) ) {
-				$error = isset( $captcha_verify['error-codes'] ) ? reset( $captcha_verify['error-codes'] ) : __( 'Error validating captcha.', 'newspack-blocks' );
-				return rest_ensure_response(
-					[
-						// Translators: error message for reCaptcha.
-						'error' => sprintf( __( 'reCaptcha error: %s', 'newspack-blocks' ), $error ),
-					]
-				);
-			}
-
-			// If the reCaptcha verification score is below our threshold for valid user input.
-			if (
-				isset( $captcha_verify['score'] ) &&
-				Newspack_Blocks::DONATE_STREAMLINED_CAPTCHA_THRESHOLD > floatval( $captcha_verify['score'] )
-			) {
-				return rest_ensure_response(
-					[
-						'error' => __( 'User action failed captcha challenge.', 'newspack-blocks' ),
+						'error' => \esc_html( $captcha_result->get_error_message() ),
 					]
 				);
 			}
 		}
 
 		$payment_metadata = [
-			'referer' => wp_get_referer(),
+			'referer' => \wp_get_referer(),
 		];
 		if ( class_exists( 'Newspack\NRH' ) && method_exists( 'Newspack\NRH', 'get_nrh_config' ) ) {
 			$nrh_config = \Newspack\NRH::get_nrh_config();
@@ -183,12 +136,12 @@ class WP_REST_Newspack_Donate_Controller extends WP_REST_Controller {
 					$full_name,
 					$frequency
 				);
-				if ( is_wp_error( $user_id ) ) {
-					return [ 'error' => wp_strip_all_tags( $user_id->get_error_message() ) ];
+				if ( \is_wp_error( $user_id ) ) {
+					return [ 'error' => \esc_html( $user_id->get_error_message() ) ];
 				}
 			}
 		} else {
-			$email_address = get_userdata( $user_id )->user_email;
+			$email_address = \get_userdata( $user_id )->user_email;
 		}
 
 		$response = \Newspack\Stripe_Connection::handle_donation(
@@ -209,6 +162,6 @@ class WP_REST_Newspack_Donate_Controller extends WP_REST_Controller {
 			]
 		);
 
-		return rest_ensure_response( $response );
+		return \rest_ensure_response( $response );
 	}
 }
