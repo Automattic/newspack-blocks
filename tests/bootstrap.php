3303f018d41f3dbc469ed8320bc57df249e94d8e
leogermani <leogermani@gmail.com>
2022-09-15 09:55:28 -0300

    test: update tests dependencies (#1266)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,5 +27,8 @@ function newspack_blocks_manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', 'newspack_blocks_manually_load_plugin' );
 
+// Load the composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
 // Start up the WP testing environment.
 require $newspack_blocks_tests_dir . '/includes/bootstrap.php';
