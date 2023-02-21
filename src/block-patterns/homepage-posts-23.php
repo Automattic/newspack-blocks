55c659b2d27bd2da01c2154d3e6b0ee562a3dfc5
Laurel Fulford <laurel.fulford@automattic.com>
2023-02-21 12:25:44 -0800

    fix: correct linting errors

--- src/block-patterns/homepage-posts-23.php
+++ src/block-patterns/homepage-posts-23.php
@@ -7,7 +7,7 @@
 
 return array(
 	'title'         => __( 'Homepage Posts Pattern 23', 'newspack-blocks' ),
-	'content'       => "<!-- wp:newspack-blocks/homepage-articles {\"showExcerpt\":false,\"minHeight\":40,\"showAvatar\":false,\"postLayout\":\"grid\",\"columns\":2,\"postsToShow\":4,\"mediaPosition\":\"behind\"} /-->",
+	'content'       => '<!-- wp:newspack-blocks/homepage-articles {"showExcerpt":false,"minHeight":40,"showAvatar":false,"postLayout":"grid","columns":2,"postsToShow":4,"mediaPosition":"behind"} /-->',
 	'viewportWidth' => 1000,
 	'categories'    => array( 'newspack-homepage-posts' ),
 );
--- src/block-patterns/homepage-posts-4.php
+++ src/block-patterns/homepage-posts-4.php
@@ -7,7 +7,7 @@
 
 return array(
 	'title'         => __( 'Homepage Posts Pattern 4', 'newspack-blocks' ),
-	'content'       => "<!-- wp:newspack-blocks/homepage-articles {\"excerptLength\":20,\"minHeight\":60,\"showAvatar\":false,\"postLayout\":\"grid\",\"columns\":2,\"postsToShow\":2,\"mediaPosition\":\"behind\"} /-->",
+	'content'       => '<!-- wp:newspack-blocks/homepage-articles {"excerptLength":20,"minHeight":60,"showAvatar":false,"postLayout":"grid","columns":2,"postsToShow":2,"mediaPosition":"behind"} /-->',
 	'viewportWidth' => 1000,
 	'categories'    => array( 'newspack-homepage-posts' ),
 );
