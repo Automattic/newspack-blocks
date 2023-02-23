7b9483a0179136a6bb401998ddd514bfad983fde
Laurel <laurel.fulford@automattic.com>
2023-02-23 11:17:03 -0800

    fix: remove unneeded group & columns blocks from patterns (#1377)

--- src/block-patterns/homepage-posts-13.php
+++ src/block-patterns/homepage-posts-13.php
@@ -7,7 +7,7 @@
 
 return array(
 	'title'         => __( 'Homepage Posts Pattern 13', 'newspack-blocks' ),
-	'content'       => "<!-- wp:group {\"className\":\"newspack-pattern homepage-posts__style-13\"} -->\n<div class=\"wp-block-group newspack-pattern homepage-posts__style-13\"><div class=\"wp-block-group__inner-container\"><!-- wp:columns -->\n<div class=\"wp-block-columns\"><!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:newspack-blocks/homepage-articles {\"showExcerpt\":false,\"minHeight\":40,\"showAuthor\":false,\"postsToShow\":1,\"mediaPosition\":\"behind\",\"typeScale\":3} /--></div>\n<!-- /wp:column -->\n\n<!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:newspack-blocks/homepage-articles {\"showExcerpt\":false,\"minHeight\":40,\"showAuthor\":false,\"postsToShow\":1,\"mediaPosition\":\"behind\",\"typeScale\":3} /--></div>\n<!-- /wp:column -->\n\n<!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:newspack-blocks/homepage-articles {\"showExcerpt\":false,\"minHeight\":40,\"showAuthor\":false,\"postsToShow\":1,\"mediaPosition\":\"behind\",\"typeScale\":3} /--></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns --></div></div>\n<!-- /wp:group -->",
+	'content'       => '<!-- wp:newspack-blocks/homepage-articles {"showExcerpt":false,"minHeight":40,"showAuthor":false,"postLayout":"grid","mediaPosition":"behind","typeScale":3} /-->',
 	'viewportWidth' => 1000,
 	'categories'    => array( 'newspack-homepage-posts' ),
 );
