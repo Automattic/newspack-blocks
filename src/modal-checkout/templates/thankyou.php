9bd10c579c7a4b9e5be993800e374e3ba145aec3
Adam Borowski <aborowski24@gmail.com>
2023-10-25 12:43:51 +0200

    feat(modal-checkout): handle failed transaction

--- src/modal-checkout/templates/thankyou.php
+++ src/modal-checkout/templates/thankyou.php
@@ -45,49 +45,61 @@ function newspack_blocks_replace_login_with_order_summary() {
 		return;
 	}
 
+	$is_success = ! $order->has_status( 'failed' );
+
 	?>
 
 	<div class="woocommerce-order">
-		<h4><?php esc_html_e( 'Transaction Successful', 'newspack-blocks' ); ?></h4>
-		<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
-			<li class="woocommerce-order-overview__date date">
-				<?php esc_html_e( 'Date:', 'newspack-blocks' ); ?>
-				<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-			</li>
-
-			<?php if ( $order->get_billing_email() ) : ?>
-				<li class="woocommerce-order-overview__email email">
-					<?php esc_html_e( 'Email:', 'newspack-blocks' ); ?>
-					<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+		<?php if ( $is_success ) : ?>
+			<h4><?php esc_html_e( 'Transaction Successful', 'newspack-blocks' ); ?></h4>
+			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+				<li class="woocommerce-order-overview__date date">
+					<?php esc_html_e( 'Date:', 'newspack-blocks' ); ?>
+					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 				</li>
-			<?php endif; ?>
 
-			<li class="woocommerce-order-overview__total total">
-				<?php esc_html_e( 'Total:', 'newspack-blocks' ); ?>
-				<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-			</li>
+				<?php if ( $order->get_billing_email() ) : ?>
+					<li class="woocommerce-order-overview__email email">
+						<?php esc_html_e( 'Email:', 'newspack-blocks' ); ?>
+						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+					</li>
+				<?php endif; ?>
 
-			<?php if ( $order->get_payment_method_title() ) : ?>
-				<li class="woocommerce-order-overview__payment-method method">
-					<?php esc_html_e( 'Payment method:', 'newspack-blocks' ); ?>
-					<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+				<li class="woocommerce-order-overview__total total">
+					<?php esc_html_e( 'Total:', 'newspack-blocks' ); ?>
+					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
 				</li>
-			<?php endif; ?>
 
-			<li class="woocommerce-order-overview__order order">
-				<?php esc_html_e( 'Transaction:', 'newspack-blocks' ); ?>
-				<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-			</li>
-		</ul>
+				<?php if ( $order->get_payment_method_title() ) : ?>
+					<li class="woocommerce-order-overview__payment-method method">
+						<?php esc_html_e( 'Payment method:', 'newspack-blocks' ); ?>
+						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					</li>
+				<?php endif; ?>
+
+				<li class="woocommerce-order-overview__order order">
+					<?php esc_html_e( 'Transaction:', 'newspack-blocks' ); ?>
+					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+			</ul>
+		<?php else : ?>
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed">
+				<?php esc_html_e( 'Unfortunately your order cannot be processed. Please attempt your purchase again.', 'newspack-blocks' ); ?>
+			</p>
+
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'newspack-blocks' ); ?></a>
+				<?php if ( is_user_logged_in() ) : ?>
+					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'newspack-blocks' ); ?></a>
+				<?php endif; ?>
+			</p>
+		<?php endif; ?>
 	</div>
 
-	<?php echo \Newspack_Blocks\Modal_Checkout::render_checkout_after_success_markup(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-
-	<?php
-	if ( $order ) {
-		echo \Newspack_Blocks\Modal_Checkout::render_newsletter_signup_form( $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
-	?>
+	<?php if ( $is_success ) : ?>
+		<?php echo \Newspack_Blocks\Modal_Checkout::render_checkout_after_success_markup(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+		<?php echo \Newspack_Blocks\Modal_Checkout::render_newsletter_signup_form( $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+	<?php endif; ?>
 
 	<?php
 }
