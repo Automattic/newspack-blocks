40d2a3b4ee46b673af051153f513ea405520509c
Miguel Peixe <miguel.peixe@a8c.com>
2023-11-22 20:07:10 -0300

    fix: style tweak

--- src/modal-checkout/templates/checkout-form.php
+++ src/modal-checkout/templates/checkout-form.php
@@ -2,17 +2,8 @@
 /**
  * Checkout Form
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-checkout.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
  * @see https://woo.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.5.0
+ * @package Newspack_Blocks
  */
 
 // phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- WooCommerce hooks.
