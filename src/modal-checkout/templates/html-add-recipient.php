c6a0d58dbd4539a673b342858319abd20da744da
dkoo <derrick.koo@automattic.com>
2023-11-22 14:09:10 -0700

    fix: add blank template for where we don’t want gifting fields

---
+++ src/modal-checkout/templates/html-add-recipient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Add recipient details.
+ * Intentionally left blank so we can control when and where to display the gift fields.
+ * Original template: https://github.com/woocommerce/woocommerce-subscriptions-gifting/blob/trunk/templates/html-add-recipient.php
+ *
+ * @package Newspack_Blocks
+ * @version 2.1
+ */
+
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- WooCommerce hooks.
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
