b1a6e673e0e82beb59f2a22038745a8c7e9a0a77
Miguel Peixe <miguel.peixe@a8c.com>
2023-12-05 16:25:18 -0300

    chore: add todo

--- src/modal-checkout/index.js
+++ src/modal-checkout/index.js
@@ -75,6 +75,11 @@ import './checkout.scss';
 		}
 
 		function handleFormError( error_message ) {
+			/**
+			 * TODO: Handle "wc-block-components-notice-banner" errors.
+			 * Example markup:
+			 * <div class=\"wc-block-components-notice-banner is-error\" role=\"alert\" data-id=\"billing_postcode\">\n\t<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\" aria-hidden=\"true\" focusable=\"false\">\n\t\t<path d=\"M12 3.2c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8 0-4.8-4-8.8-8.8-8.8zm0 16c-4 0-7.2-3.3-7.2-7.2C4.8 8 8 4.8 12 4.8s7.2 3.3 7.2 7.2c0 4-3.2 7.2-7.2 7.2zM11 17h2v-6h-2v6zm0-8h2V7h-2v2z\"><\/path>\n\t<\/svg>\n\t<div class=\"wc-block-components-notice-banner__content\">\n\t\t<strong>Billing ZIP Code<\/strong> is a required field.\t<\/div>\n<\/div>\n
+			 */
 			// Clear previous errors.
 			$(
 				'.woocommerce-NoticeGroup-checkout, .woocommerce-error, .woocommerce-message, .wc-block-components-notice-banner'
