8195239d11de8814a056292f23b10e157e2bd341
Rasmy Nguyen <rtnguyen87@gmail.com>
2024-01-30 14:19:54 -0500

    chore(modal-checkout): remove redundant templates

--- includes/class-modal-checkout.php
+++ includes/class-modal-checkout.php
@@ -605,9 +605,7 @@ final class Modal_Checkout {
 			// Replace the login form with the order summary if using the modal checkout. This is
 			// for the case where the reader used an existing email address.
 			$custom_templates['global/form-login.php']       = 'src/modal-checkout/templates/thankyou.php';
-			$custom_templates['checkout/form-billing.php']   = 'src/modal-checkout/templates/form-billing.php';
 			$custom_templates['checkout/form-checkout.php']  = 'src/modal-checkout/templates/form-checkout.php';
-			$custom_templates['checkout/form-shipping.php']  = 'src/modal-checkout/templates/form-shipping.php';
 			$custom_templates['checkout/payment-method.php'] = 'src/modal-checkout/templates/payment-method.php';
 			$custom_templates['checkout/thankyou.php']       = 'src/modal-checkout/templates/thankyou.php';
 		}
