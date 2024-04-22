6b877220a7bf1424c99bd6520427acd51a42fbdb
Laurel Fulford <laurel.fulford@automattic.com>
2024-04-22 08:38:50 -0700

    feat: update close button classes

--- includes/class-modal-checkout.php
+++ includes/class-modal-checkout.php
@@ -250,7 +250,7 @@ final class Modal_Checkout {
 			<div class="<?php echo esc_attr( "{$class_prefix}__modal" ); ?>">
 				<header class="<?php echo esc_attr( "{$class_prefix}__modal__header" ); ?>">
 					<h2><?php echo esc_html( $title ); ?></h2>
-					<button class="<?php echo esc_attr( "{$class_prefix}__modal__close" ); ?>">
+					<button class="<?php echo esc_attr( "{$class_prefix}__button {$class_prefix}__button--icon {$class_prefix}__button--ghost {$class_prefix}__modal__close" ); ?>">
 						<span class="screen-reader-text"><?php esc_html_e( 'Close', 'newspack-blocks' ); ?></span>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false">
 							<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
