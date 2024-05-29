818d89d1f1cc1da2a4fc65178d2f02512ce791ee
Thomas Guillot <thomasguillot@users.noreply.github.com>
2024-05-29 08:02:39 +0100

    feat: update donations patterns to match subscription pattern; add squeeze pattern (#1752)

--- src/block-patterns/donations-1.php
+++ src/block-patterns/donations-1.php
@@ -7,7 +7,7 @@
 
 return array(
 	'title'         => __( 'Donations Pattern 1', 'newspack-blocks' ),
-	'content'       => "<!-- wp:columns {\"align\":\"wide\"} -->\n<div class=\"wp-block-columns alignwide\"><!-- wp:column {\"verticalAlignment\":\"center\",\"width\":25} -->\n<div class=\"wp-block-column is-vertically-aligned-center\" style=\"flex-basis:25%\"><!-- wp:heading -->\n<h2>" . __( 'Support our publication', 'newspack-blocks' ) . "</h2>\n<!-- /wp:heading --></div>\n<!-- /wp:column -->\n\n<!-- wp:column {\"width\":75} -->\n<div class=\"wp-block-column\" style=\"flex-basis:75%\"><!-- wp:newspack-blocks/donate {\"className\":\"is-style-modern\"} /--></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns -->",
+	'content'       => "<!-- wp:columns {\"align\":\"wide\"} -->\n<div class=\"wp-block-columns alignwide\"><!-- wp:column {\"verticalAlignment\":\"center\",\"width\":25} -->\n<div class=\"wp-block-column is-vertically-aligned-center\" style=\"flex-basis:25%\"><!-- wp:heading -->\n<h2>" . $donations_title . "</h2>\n<!-- /wp:heading --></div>\n<!-- /wp:column -->\n\n<!-- wp:column {\"width\":75} -->\n<div class=\"wp-block-column\" style=\"flex-basis:75%\"><!-- wp:newspack-blocks/donate {\"className\":\"is-style-modern\",\"buttonColor\":\"#6c6c6c\"} /--></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns -->",
 	'viewportWidth' => 910,
 	'categories'    => array( 'newspack-donations' ),
 );
