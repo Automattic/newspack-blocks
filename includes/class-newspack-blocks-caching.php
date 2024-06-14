62ba1439e420420cca940fab9c863d58fc2d0b70
claudiulodro <claudiulodro@gmail.com>
2024-06-14 13:16:45 -0700

    feat: block caching

---
+++ includes/class-newspack-blocks-caching.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Newspack block caching layer
+ *
+ * @package Newspack_Blocks
+ */
+
+/**
+ * Cache dynamic blocks for improved performance.
+ */
+class Newspack_Blocks_Caching {
+	/**
+	 * Add hooks and filters.
+	 */
+	public static function init() {
+		if ( defined( 'NEWSPACK_CACHE_BLOCKS' ) && NEWSPACK_CACHE_BLOCKS ) {
+			add_filter( 'pre_render_block', [ __CLASS__, 'maybe_serve_cached_block' ], 10, 2 );
+			add_filter( 'render_block', [ __CLASS__, 'maybe_cache_block' ], 9999, 2 );
+		}
+	}
+
+	/**
+	 * Determine whether a block should be cached.
+	 *
+	 * @param array $block_data Parsed block data.
+	 * @return bool True if block should be cached. False otherwise.
+	 */
+	protected static function should_cache_block( $block_data ) {
+		$cacheable_blocks = [
+			'newspack-blocks/homepage-articles',
+			'newspack-blocks/carousel',
+		];
+		$cacheable_blocks = apply_filters( 'newspack_blocks_cacheable_blocks', $cacheable_blocks );
+		return in_array( $block_data['blockName'], $cacheable_blocks, true );
+	}
+
+	/**
+	 * Get the cache key for a block's cache.
+	 *
+	 * @param array $block_data Parsed block data.
+	 * @return string Cache key.
+	 */
+	protected static function get_cache_key( $block_data ) {
+		$block_attributes = $block_data['attrs'];
+
+		// We're using a heuristic here to increase the rate of cache hits with very limited downside.
+		// Pages should each have their own cache, because they are likely a landing page with various article blocks.
+		// Posts and other publicly_queryable post types should all share a cache, because 99% of the time article blocks
+		// are in the sidebar, below-content, or (if within content) fetching specific posts. We want an article block in
+		// the e.g. sidebar to be served from cache across all posts.
+		// The tradeoff is that occasionally the current post may show up in an article block on a post.
+		// Archives and non-singular should all use the global cache, because there is nothing that would need de-duplication.
+		if ( is_singular() ) {
+			$post_type        = get_post_type();
+			$post_type_object = get_post_type_object( $post_type );
+			if ( ! $post_type_object->publicly_queryable ) {
+				$block_attributes['parent_post'] = get_the_ID();
+			}
+		}
+
+		$cache_key = 'np_cached_block_' . md5( wp_json_encode( $block_attributes ) );
+		return $cache_key;
+	}
+
+	/**
+	 * Debug logging for observing cache behavior.
+	 *
+	 * @param string $message Message to log.
+	 */
+	protected static function debug_log( $message ) {
+		if ( defined( 'NEWSPACK_LOG_LEVEL' ) && (int) NEWSPACK_LOG_LEVEL >= 2 ) {
+			Newspack\Logger::log( $message );
+		}
+	}
+
+	/**
+	 * Serve a cached block if a valid one exists.
+	 *
+	 * @param string|null $block_html Block HTML. If you return something non-null here it will short-circuit block rendering.
+	 * @param array       $block_data Parsed block data.
+	 * @return string|null Block markup if served from cache. Default (usually null), otherwise.
+	 */
+	public static function maybe_serve_cached_block( $block_html, $block_data ) {
+		if ( ! self::should_cache_block( $block_data ) ) {
+			return $block_html;
+		}
+
+		$cache_key = self::get_cache_key( $block_data );
+		self::debug_log( sprintf( 'Checking cache for: %s', $cache_key ) );
+
+		$cached_data = wp_cache_get( $cache_key );
+		if ( ! is_array( $cached_data ) || ! isset( $cached_data['timestamp_generated'], $cached_data['cached_content'] ) ) {
+			self::debug_log( sprintf( 'Cached data not found for: %s', $cache_key ) );
+			return $block_html;
+		}
+
+		// Check to see if cache needs to be refreshed.
+		// We're using WP_Query so that the query here gets cached for further cache checks.
+		$latest_update = get_posts(
+			[
+				'post_type'      => 'post',
+				'post_status'    => 'publish',
+				'orderby'        => 'modified',
+				'order'          => 'DESC',
+				'posts_per_page' => 1,
+			]
+		);
+		if ( empty( $latest_update ) ) {
+			return $block_html;
+		}
+
+		$last_update_timestamp = strtotime( $latest_update[0]->post_modified_gmt );
+		if ( $last_update_timestamp > $cached_data['timestamp_generated'] ) {
+			self::debug_log( sprintf( 'A post has been updated since %s was cached. Not serving from cache.', $cache_key ) );
+			return $block_html;
+		}
+
+		self::debug_log( sprintf( 'Serving cached block: %s', $cache_key ) );
+
+		Newspack_Blocks::enqueue_view_assets( 'homepage-articles' );
+
+		return $cached_data['cached_content'];
+	}
+
+	/**
+	 * Save the block markup to cache. If we've reached this function, that means that the
+	 * rendering wasn't short-circuited by a cached version, so we always cache here.
+	 *
+	 * @param string $block_html Block markup ready for rendering.
+	 * @param array  $block_data Parsed block data.
+	 * @return string Unmodified $block_html.
+	 */
+	public static function maybe_cache_block( $block_html, $block_data ) {
+		if ( ! self::should_cache_block( $block_data ) ) {
+			return $block_html;
+		}
+
+		$cache_key = self::get_cache_key( $block_data );
+
+		$cache_data = [
+			'timestamp_generated' => time(),
+			'cached_content'      => $block_html,
+		];
+		wp_cache_set( $cache_key, $cache_data );
+
+		self::debug_log( sprintf( 'Caching block: %s', $cache_key ) );
+
+		return $block_html;
+	}
+}
+Newspack_Blocks_Caching::init();
