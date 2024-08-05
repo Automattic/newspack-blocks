8e16f67501fd74fa3d5360e973d0abc2b8cd5e74
Miguel Peixe <miguel.peixe@a8c.com>
2024-08-05 10:30:05 -0300

    chore: rename subscribe contact method

--- includes/class-modal-checkout.php
+++ includes/class-modal-checkout.php
@@ -929,7 +929,7 @@ final class Modal_Checkout {
 			if ( empty( $signup_data['lists'] ) ) {
 				return new \WP_Error( 'newspack_no_lists_selected', __( 'No lists selected.', 'newspack-blocks' ) );
 			} else {
-				$result = \Newspack_Newsletters_Contacts::upsert(
+				$result = \Newspack_Newsletters_Contacts::subscribe_contact(
 					[
 						'email'    => $signup_data['email'],
 						'metadata' => [
