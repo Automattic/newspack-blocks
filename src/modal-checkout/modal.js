2f16c9711ef764912045e68d01114c1d4a4ce42f
Rasmy Nguyen <raz@automattic.com>
2024-08-29 10:48:55 -0400

    Revert "fix(ras-acc): ensure variable modal adds to ras overlays"

--- src/modal-checkout/modal.js
+++ src/modal-checkout/modal.js
@@ -50,8 +50,8 @@ domReady( () => {
 	const initialHeight = modalContent.clientHeight + spinner.clientHeight + 'px';
 	const closeCheckout = () => {
 		const container = iframe?.contentDocument?.querySelector( `#${ IFRAME_CONTAINER_ID }` );
-		const afterSuccessUrlInput = container?.querySelector( 'input[name="after_success_url"]' );
-		const afterSuccessBehaviorInput = container?.querySelector(
+		const afterSuccessUrlInput = container.querySelector( 'input[name="after_success_url"]' );
+		const afterSuccessBehaviorInput = container.querySelector(
 			'input[name="after_success_behavior"]'
 		);
 		const hasNewsletterPopup = document?.querySelector( '.newspack-newsletters-signup-modal' );
@@ -73,7 +73,12 @@ domReady( () => {
 				iframeResizeObserver.disconnect();
 			}
 
-			document.querySelectorAll( `.${ MODAL_CLASS_PREFIX }-container` ).forEach( el => closeModal( el ) );
+			document.querySelectorAll( `.${ MODAL_CLASS_PREFIX }-container` ).forEach( el => {
+				closeModal( el );
+				if ( el.overlayId && window.newspackReaderActivation?.overlays ) {
+					window.newspackReaderActivation?.overlays.remove( el.overlayId );
+				}
+			} );
 
 			if ( modalTrigger ) {
 				modalTrigger.focus();
@@ -117,6 +122,11 @@ domReady( () => {
 	const openCheckout = () => {
 		spinner.style.display = 'flex';
 		openModal( modalCheckout );
+
+		if ( window.newspackReaderActivation?.overlays ) {
+			modalCheckout.overlayId = window.newspackReaderActivation?.overlays.add();
+		}
+
 		modalContent.appendChild( iframe );
 		modalCheckout.addEventListener( 'click', ev => {
 			if ( ev.target === modalCheckout ) {
@@ -128,17 +138,11 @@ domReady( () => {
 	};
 
 	const closeModal = el => {
-		if ( el.overlayId && window.newspackReaderActivation?.overlays ) {
-			window.newspackReaderActivation?.overlays.remove( el.overlayId );
-		}
 		el.setAttribute( 'data-state', 'closed' );
 		document.body.style.overflow = 'auto';
 	};
 
 	const openModal = el => {
-		if ( window.newspackReaderActivation?.overlays ) {
-			modalCheckout.overlayId = window.newspackReaderActivation?.overlays.add();
-		}
 		el.setAttribute( 'data-state', 'open' );
 		document.body.style.overflow = 'hidden';
 	};
