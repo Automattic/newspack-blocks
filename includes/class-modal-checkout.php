dd46fcb9e8153d5e7ccebd1f3637f255abaac069
Rasmy Nguyen <raz@automattic.com>
2024-10-11 17:10:33 -0400

    fix(ras-acc): correct checks when rendering modal checkout template (#1902)
    This fixes two small checks when rendering the modal checkout template:
    
    - Checks for cart and checkout pages in addition to wc endpoints
    - Checks for the existence of `notice` key before using

--- includes/class-modal-checkout.php
+++ includes/class-modal-checkout.php
@@ -681,8 +681,12 @@ final class Modal_Checkout {
 		if ( ! self::is_modal_checkout() ) {
 			return $template;
 		}
-		// Ensure we are on a wc endpoint.
-		if ( ! function_exists( 'is_wc_endpoint_url' ) && ! is_wc_endpoint_url() ) {
+		// Ensure we are on a wc page or endpoint.
+		if (
+			( ! function_exists( 'is_checkout' ) && ! is_checkout() ) &&
+			( ! function_exists( 'is_cart' ) && ! is_cart() ) &&
+			( ! function_exists( 'is_wc_endpoint_url' ) && ! is_wc_endpoint_url() )
+		) {
 			return $template;
 		}
 		$class_prefix = self::get_class_prefix();
@@ -711,7 +715,7 @@ final class Modal_Checkout {
 							$message = __( "We're sorry, there was an unexpected error. Please try again in a few minutes.", 'newspack-blocks' );
 							if ( ! empty( $wc_errors ) ) {
 								$error = $wc_errors[0];
-								if ( is_array( $error ) ) {
+								if ( is_array( $error ) && isset( $error['notice'] ) ) {
 									$message = $error['notice'];
 								}
 							}
