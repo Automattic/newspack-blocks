cbe0cdb805354ae9dea6c0c35659d401198a30c2
Laurel <laurel.fulford@automattic.com>
2024-10-22 11:06:33 -0700

    fix: add check if product can be purchased (#1916)

--- src/blocks/checkout-button/view.php
+++ src/blocks/checkout-button/view.php
@@ -33,9 +33,35 @@ function render_callback( $attributes, $content ) {
 		return '';
 	}
 	$product_id = $attributes['product'];
+
+	if ( function_exists( 'wc_get_product' ) ) {
+		$product = wc_get_product( $product_id );
+
+		// Check if product can actually be purchased before rendering.
+		if ( ! $product || ! $product->is_purchasable() ) {
+			return '';
+		}
+
+		// Check if product is NYP with no prices set at all.
+		if ( class_exists( '\WC_Name_Your_Price_Helpers' ) && \WC_Name_Your_Price_Helpers::is_nyp( $product_id ) ) {
+			$price = $product->get_price();
+			if ( ! empty( $attributes['price'] ) ) {
+				// Default to the price set in the block attributes.
+				$price = $attributes['price'];
+			}
+
+			$suggested_price = \WC_Name_Your_Price_Helpers::get_suggested_price( $product_id );
+			$minimum_price   = \WC_Name_Your_Price_Helpers::get_minimum_price( $product_id );
+			if ( empty( $price ) && empty( $suggested_price ) && empty( $minimum_price ) ) {
+				return '';
+			}
+		}
+	}
+
 	if ( $attributes['is_variable'] && ! empty( $attributes['variation'] ) ) {
 		$product_id = $attributes['variation'];
 	}
+
 	\Newspack_Blocks\Modal_Checkout::enqueue_modal( $product_id );
 	\Newspack_Blocks::enqueue_view_assets( 'checkout-button' );
 	return $content;
