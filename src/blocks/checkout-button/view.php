0d80c922805e294a475d6d7ce00b198ba960665e
Laurel Fulford <laurel.fulford@automattic.com>
2024-10-22 10:02:24 -0700

    fix: add check for price-less NYP products on front-end

--- src/blocks/checkout-button/view.php
+++ src/blocks/checkout-button/view.php
@@ -34,11 +34,27 @@ function render_callback( $attributes, $content ) {
 	}
 	$product_id = $attributes['product'];
 
-	// Check if product can actually be purchased before rendering.
 	if ( function_exists( 'wc_get_product' ) ) {
 		$product = wc_get_product( $product_id );
+
+		// Check if product can actually be purchased before rendering.
 		if ( ! $product || ! $product->is_purchasable() ) {
 			return '';
+		}
+
+		// Check if product is NYP with no prices set at all.
+		if ( class_exists( '\WC_Name_Your_Price_Helpers' ) && \WC_Name_Your_Price_Helpers::is_nyp( $product_id ) ) {
+			$price = $product->get_price();
+			if ( ! empty( $attributes['price'] ) ) {
+				// Default to the price set in the block attributes.
+				$price = $attributes['price'];
+			}
+
+			$suggested_price = \WC_Name_Your_Price_Helpers::get_suggested_price( $product_id );
+			$minimum_price   = \WC_Name_Your_Price_Helpers::get_minimum_price( $product_id );
+			if ( empty( $price ) && empty( $suggested_price ) && empty( $minimum_price ) ) {
+				return '';
+			}
 		}
 	}
 
