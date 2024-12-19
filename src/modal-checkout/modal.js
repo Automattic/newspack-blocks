df5f044aad567483890ca8c6bbb10bcc4a493dbc
Laurel Fulford <laurel.fulford@automattic.com>
2024-12-19 14:16:01 -0800

    feat: fix registration prompt location

--- src/modal-checkout/modal.js
+++ src/modal-checkout/modal.js
@@ -283,22 +283,20 @@ domReady( () => {
 			}
 		}
 		// Populate cart and redirect to checkout if there is an unsupported payment gateway.
-		if ( ! isModalCheckout ) {
-			if ( ! shouldPromptRegistration() ) {
+		if ( ! isModalCheckout && ! shouldPromptRegistration() ) {
 				generateCart( formData ).then( url => {
 					window.location.href = url.replace( /&?modal_checkout=1/, '' );
 				} );
-			}
-			// Add some animation to the Checkout Button and Donate block while the non-modal checkout is loading.
-			// For now, don't do it when any popup opens, just when we go right to the checkout page.
-			if ( ! ( formData.get( 'is_variable' ) && ! formData.get( 'variation_id' ) ) && ! shouldPromptRegistration() ) {
-				const buttons = form.querySelectorAll( 'button[type=submit]' );
-				buttons.forEach( button => {
-					button.classList.add( 'non-modal-checkout-loading' );
-					const buttonText = button.innerHTML;
-					button.innerHTML = '<span>' + buttonText + '</span>';
-				} );
-			}
+				// Add some animation to the Checkout Button and Donate block while the non-modal checkout is loading.
+				// For now, don't do it when any popup opens, just when we go right to the checkout page.
+				if ( ! ( formData.get( 'is_variable' ) && ! formData.get( 'variation_id' ) ) && ! shouldPromptRegistration() ) {
+					const buttons = form.querySelectorAll( 'button[type=submit]' );
+					buttons.forEach( button => {
+						button.classList.add( 'non-modal-checkout-loading' );
+						const buttonText = button.innerHTML;
+						button.innerHTML = '<span>' + buttonText + '</span>';
+					} );
+				}
 			return;
 		}
 		form.classList.remove( 'modal-processing' );
