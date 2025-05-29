3047c57379300f6da55c23e9897912f12fc72bf2
Adam Cassis <adam.cassis@automattic.com>
2025-05-29 12:26:07 +0200

    fix(donate-block): amount formatting

--- src/blocks/donate/frontend/class-newspack-blocks-donate-renderer-base.php
+++ src/blocks/donate/frontend/class-newspack-blocks-donate-renderer-base.php
@@ -148,7 +148,7 @@ abstract class Newspack_Blocks_Donate_Renderer_Base {
 		if ( isset( $configuration['minimumDonation'] ) ) {
 			foreach ( $configuration['amounts'] as $frequency => $amounts ) {
 				foreach ( $amounts as $index => $amount ) {
-					$configuration['amounts'][ $frequency ][ $index ] = max( $configuration['minimumDonation'], $amount );
+					$configuration['amounts'][ $frequency ][ $index ] = max( (float) $configuration['minimumDonation'], $amount );
 				}
 			}
 		}
