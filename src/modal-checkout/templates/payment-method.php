21f1828c406f461d7a399f0ac3df02260c7c9087
Rasmy Nguyen <raz@automattic.com>
2025-08-21 17:22:36 -0400

    fix(modal-checkout): support stripe optimized checkout

--- src/modal-checkout/templates/payment-method.php
+++ src/modal-checkout/templates/payment-method.php
@@ -16,16 +16,16 @@ if ( ! empty( $subscription ) ) {
 ?>
 
 <li class="wc_payment_method payment_method_<?php echo esc_attr( $gateway->id ); ?>">
-	<label class="newspack-ui__input-card" for="payment_method_<?php echo esc_attr( $gateway->id ); ?>">
+	<span class="newspack-ui__input-card">
 		<input id="payment_method_<?php echo esc_attr( $gateway->id ); ?>" type="radio" class="input-radio <?php echo $supports_payment_method_changes ? 'supports-payment-method-changes' : ''; ?>" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
-		<span>
+		<label for="payment_method_<?php echo esc_attr( $gateway->id ); ?>">
 			<?php echo $gateway->get_title(); /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?> <?php echo $gateway->get_icon(); /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?>
-			<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
-				<div class="payment_box payment_method_<?php echo esc_attr( $gateway->id ); ?>" <?php if ( ! $gateway->chosen ) : /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>style="display:none;"<?php endif; /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>>
-					<?php $gateway->payment_fields(); ?>
-					<?php do_action( 'newspack_blocks_after_payment_fields', $gateway->id ); ?>
-				</div>
-			<?php endif; ?>
-		</span>
-	</label>
+		</label>
+		<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
+			<div class="payment_box payment_method_<?php echo esc_attr( $gateway->id ); ?>" <?php if ( ! $gateway->chosen ) : /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>style="display:none;"<?php endif; /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>>
+				<?php $gateway->payment_fields(); ?>
+				<?php do_action( 'newspack_blocks_after_payment_fields', $gateway->id ); ?>
+			</div>
+		<?php endif; ?>
+	</span>
 </li>
