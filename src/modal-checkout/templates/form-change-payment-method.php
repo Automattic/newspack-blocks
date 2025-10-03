d02039589ceafc9bc62b7207c50b3e581ee69ef8
Miguel Peixe <miguel.peixe@automattic.com>
2025-10-03 13:35:10 -0300

    fix(modal-checkout): style improvements to single-gateway

--- src/modal-checkout/templates/form-change-payment-method.php
+++ src/modal-checkout/templates/form-change-payment-method.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<form id="order_review" method="post">
+<form id="order_review" class="change-payment-method" method="post">
 
 	<div id="payment">
 		<?php
