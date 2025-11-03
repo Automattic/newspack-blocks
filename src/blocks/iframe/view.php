3060af4d00300007b7f3ae88d6d044e0e66dcb0c
dkoo <derrick.koo@automattic.com>
2025-11-03 14:29:58 -0700

    fix: handle multiple responsive iframe blocks

--- src/blocks/iframe/view.php
+++ src/blocks/iframe/view.php
@@ -110,44 +110,6 @@ function newspack_blocks_get_iframe_html( $mode, $src, $height, $width, $is_full
 			></iframe>
 		</div>
 	</figure>
-
-	<script>
-		const iframe = document.getElementById("<?php echo esc_attr( $iframe_id ); ?>");
-		const timerId = setInterval( function() {
-				iframe.src = iframe.src;
-			}, 2000 );
-
-		iframe.onload = function() {
-			clearInterval(timerId);
-		}
-
-		// Add a listener for dynamic resizing if the iframe supports it.
-		window.addEventListener("message", function(event) {
-			// Reject messages from untrusted origins.
-			if (event.origin !== new URL(iframe.src).origin || iframe.contentWindow !== event.source) {
-				return; 
-			}
-
-			var iframeHeight = 0;
-			if (event.data && event.data.height){
-				if (typeof event.data.height === "number") {
-					iframeHeight = event.data.height;
-				} else if (typeof event.data.height === "string") {
-					iframeHeight = Number(event.data.height);
-				}
-			}
-			if (!isNaN(iframeHeight) && iframeHeight > 0) {
-				// Remove height from the iframe's parent element if needed.
-				if (iframe.parentElement && iframe.parentElement.style.height !== "auto") {
-					iframe.parentElement.style.height = "auto";
-				}
-
-				// Set the new height dynamically.
-				iframe.style.height = iframeHeight + "px";
-			}
-		});
-	</script>
-
 	<?php
 	return ob_get_clean();
 }
