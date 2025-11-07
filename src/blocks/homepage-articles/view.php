5e52ed56699621ccc5544fe2c21b400826ec4394
leogermani <leogermani@gmail.com>
2025-11-07 17:23:02 -0300

    fix: content loop load more for custom taxs

--- src/blocks/homepage-articles/view.php
+++ src/blocks/homepage-articles/view.php
@@ -323,19 +323,6 @@ function newspack_blocks_render_block_homepage_articles( $attributes ) {
 		$styles = 'color: ' . $attributes['customTextColor'] . ';';
 	}
 
-	// Handle custom taxonomies.
-	if ( isset( $attributes['customTaxonomies'] ) ) {
-		$custom_taxes = $attributes['customTaxonomies'];
-		unset( $attributes['customTaxonomies'] );
-		if ( is_array( $custom_taxes ) && ! empty( $custom_taxes ) ) {
-			foreach ( $custom_taxes as $tax ) {
-				if ( ! empty( $tax['slug'] ) && ! empty( $tax['terms'] ) ) {
-					$attributes[ $tax['slug'] ] = $tax['terms'];
-				}
-			}
-		}
-	}
-
 	$articles_rest_url = add_query_arg(
 		array_merge(
 			map_deep(
