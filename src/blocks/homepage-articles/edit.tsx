b0f241613d231f637f10d6ae9e35f06116582bca
Jason Lee <jason.lee@automattic.com>
2026-05-26 01:12:14 -0700

    fix: neutralize tag-label preview anchors in editor

--- src/blocks/homepage-articles/edit.tsx
+++ src/blocks/homepage-articles/edit.tsx
@@ -118,7 +118,7 @@ class Edit extends Component< HomepageArticlesProps > {
 						<div className="cat-links tag-labels">
 							{ post.newspack_tag_labels.map( ( newspack_tag_label, index ) => {
 								return newspack_tag_label.link ? (
-									<a key={ index } href={ newspack_tag_label.link } className="tag-label flag">
+									<a key={ index } href="#" className="tag-label flag">
 										{ newspack_tag_label.flag }
 									</a>
 								) : (
